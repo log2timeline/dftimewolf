cd72eff405340d6c006301d7b059c35ab0ab6350
Thomas Chopitea <tomchop@gmail.com>
2017-10-10 16:36:41 -0700

    Recipe that leverages GRRHuntDownloader Downloads results of a hunt, runs plaso, and exports to timesketch.

---
+++ dftimewolf/cli/recipes/grr_huntresults_plaso_timesketch.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""DFTimewolf recipe for collecting data from the filesystem.
+
+- Collectors collect from a path in the FS
+- Processes them with a local install of plaso
+- Exports them to a new Timesketch sketch
+"""
+
+from __future__ import unicode_literals
+
+contents = {
+    'name': 'grr_huntresults_plaso_timesketch',
+    'params': {},
+    'collectors': [{
+        'name': 'GRRHuntDownloader',
+        'args': {
+            'hunt_id': '@hunt_id',
+            'grr_server_url': 'http://localhost:8000',
+            'grr_auth': ('admin', 'admin'),
+            'verbose': True,
+            'reason': '@reason',
+        },
+    }],
+    'processors': [{
+        'name': 'LocalPlasoProcessor',
+        'args': {
+            'timezone': None,
+            'verbose': True,
+        },
+    }],
+    'exporters': [{
+        'name': 'TimesketchExporter',
+        'args': {
+            'ts_endpoint': '@ts_endpoint',
+            'ts_username': '@ts_username',
+            'ts_password': '@ts_password',
+            'incident_id': '@incident_id',
+            'sketch_id': None,
+            'verbose': True,
+        }
+    }],
+}
+
+args = [
+    ('hunt_id', 'ID of GRR Hunt results to fetch'),
+    ('reason', 'Reason for exporting hunt (used for Timesketch description)'),
+]
