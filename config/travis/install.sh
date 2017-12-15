2b9aae5a28d547a701d111a0d3dbf2b9dd959a47
Joachim Metz <joachim.metz@gmail.com>
2017-12-15 14:36:19 +0100

    Changes for dependency management and deployment. (#37)

---
+++ config/travis/install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# Script to set up Travis-CI test VM.
+
+COVERALL_DEPENDENCIES="python-coverage python-coveralls python-docopt";
+
+L2TBINARIES_DEPENDENCIES="certifi chardet idna pytz requests urllib3";
+
+L2TBINARIES_TEST_DEPENDENCIES="funcsigs mock pbr six";
+
+PYTHON2_DEPENDENCIES="python-certifi python-chardet python-idna python-requests python-tz python-urllib3";
+
+PYTHON2_TEST_DEPENDENCIES="python-mock python-tox";
+
+# Exit on error.
+set -e;
+
+if test ${TRAVIS_OS_NAME} = "osx";
+then
+	git clone https://github.com/log2timeline/l2tdevtools.git;
+
+	mv l2tdevtools ../;
+	mkdir dependencies;
+
+	PYTHONPATH=../l2tdevtools ../l2tdevtools/tools/update.py --download-directory dependencies --track dev ${L2TBINARIES_DEPENDENCIES} ${L2TBINARIES_TEST_DEPENDENCIES};
+
+elif test ${TRAVIS_OS_NAME} = "linux";
+then
+	sudo rm -f /etc/apt/sources.list.d/travis_ci_zeromq3-source.list;
+
+	sudo add-apt-repository ppa:gift/dev -y;
+	sudo apt-get update -q;
+	# Only install the Python 2 dependencies.
+	# Also see: https://docs.travis-ci.com/user/languages/python/#Travis-CI-Uses-Isolated-virtualenvs
+	sudo apt-get install -y ${COVERALL_DEPENDENCIES} ${PYTHON2_DEPENDENCIES} ${PYTHON2_TEST_DEPENDENCIES};
+fi
--- setup.py
+++ setup.py
@@ -1,26 +1,130 @@
-"""This is the setup file for the project. The standard setup rules apply:
-   python setup.py build
-   sudo python setup.py install
-"""
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Installation and deployment script."""
 
-from setuptools import find_packages
-from setuptools import setup
+from __future__ import print_function
+import sys
+
+try:
+  from setuptools import find_packages, setup
+except ImportError:
+  from distutils.core import find_packages, setup
+
+try:
+  from distutils.command.bdist_rpm import bdist_rpm
+except ImportError:
+  bdist_rpm = None
+
+if sys.version < '2.7':
+  print('Unsupported Python version: {0:s}.'.format(sys.version))
+  print('Supported Python versions are 2.7 or a later 2.x version.')
+  sys.exit(1)
+
+# Change PYTHONPATH to include dftimewolf so that we can get the version.
+sys.path.insert(0, '.')
+
+import dftimewolf  # pylint: disable=wrong-import-position
 
 
-def readme():
-  """Reads README and returns its contents."""
-  with open('readme.md') as f:
-    return f.read()
+if not bdist_rpm:
+  BdistRPMCommand = None
+else:
+  class BdistRPMCommand(bdist_rpm):
+    """Custom handler for the bdist_rpm command."""
+
+    def _make_spec_file(self):
+      """Generates the text of an RPM spec file.
+
+      Returns:
+        list[str]: lines of the RPM spec file.
+      """
+      # Note that bdist_rpm can be an old style class.
+      if issubclass(BdistRPMCommand, object):
+        spec_file = super(BdistRPMCommand, self)._make_spec_file()
+      else:
+        spec_file = bdist_rpm._make_spec_file(self)
+
+      if sys.version_info[0] < 3:
+        python_package = 'python'
+      else:
+        python_package = 'python3'
+
+      description = []
+      summary = ''
+      in_description = False
+
+      python_spec_file = []
+      for line in iter(spec_file):
+        if line.startswith('Summary: '):
+          summary = line
+
+        elif line.startswith('BuildRequires: '):
+          line = 'BuildRequires: {0:s}-setuptools'.format(python_package)
+
+        elif line.startswith('Requires: '):
+          if python_package == 'python3':
+            line = line.replace('python', 'python3')
+
+        elif line.startswith('%description'):
+          in_description = True
+
+        elif line.startswith('%files'):
+          # Cannot use %{_libdir} here since it can expand to "lib64".
+          lines = [
+              '%files',
+              '%defattr(644,root,root,755)',
+              '%doc ACKNOWLEDGEMENTS AUTHORS LICENSE README',
+              '%{_prefix}/lib/python*/site-packages/dftimewolf/*.py',
+              '%{_prefix}/lib/python*/site-packages/dftimewolf*.egg-info/*',
+              '%exclude %{_prefix}/lib/python*/site-packages/dftimewolf/*.pyc',
+              '%exclude %{_prefix}/lib/python*/site-packages/dftimewolf/*.pyo',
+              ('%exclude %{_prefix}/lib/python*/site-packages/dftimewolf/'
+               '__pycache__/*')]
+
+          python_spec_file.extend(lines)
+          break
+
+        elif line.startswith('%prep'):
+          in_description = False
+
+          python_spec_file.append(
+              '%package -n {0:s}-%{{name}}'.format(python_package))
+          python_spec_file.append('{0:s}'.format(summary))
+          python_spec_file.append('')
+          python_spec_file.append(
+              '%description -n {0:s}-%{{name}}'.format(python_package))
+          python_spec_file.extend(description)
+
+        elif in_description:
+          # Ignore leading white lines in the description.
+          if not description and not line:
+            continue
+
+          description.append(line)
+
+        python_spec_file.append(line)
+
+      return python_spec_file
+
+
+dftimewolf_description = (
+    'Digital forensic orchestration.')
+
+dftimewolf_long_description = (
+    'dfTimeWolf, a framework for orchestrating forensic collection, processing '
+    'and data export.')
 
 setup(
     name='dftimewolf',
-    version='2017.06',
-    description='Digital forensic orchestration',
-    long_description=readme(),
+    version=dftimewolf.__version__,
+    description=dftimewolf_description,
+    long_description=dftimewolf_long_description,
     url='https://github.com/log2timeline/dftimewolf',
     author='DFTimewolf development team',
     license='Apache License, Version 2.0',
     packages=find_packages(),
+    cmdclass={
+        'bdist_rpm': BdistRPMCommand},
     classifiers=[
         'Development Status :: 4 - Beta',
         'Operating System :: OS Independent',
