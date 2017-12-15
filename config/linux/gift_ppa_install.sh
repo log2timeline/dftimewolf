960d1c78fd7b99f3a4c4a33e642895d1b164c431
Daniel White <onager@deerpie.com>
2017-12-15 15:36:23 +0100

    Add pip

--- config/linux/gift_ppa_install.sh
+++ config/linux/gift_ppa_install.sh
@@ -24,7 +24,8 @@ sudo apt-get update -q
 sudo apt-get install -y ${PYTHON2_DEPENDENCIES}
 
 # Pending resolution of https://github.com/log2timeline/l2tdevtools/issues/233.
-sudo pip install grr-api-client
+sudo apt-get install -y python-pip
+sudo pip install -y grr-api-client
 
 if [[ "$*" =~ "include-development" ]]; then
     sudo apt-get install -y ${DEVELOPMENT_DEPENDENCIES}
