21b1046cc4cfde45909c05d19ce566e5d86a2af8
Thomas Chopitea <tomchop@gmail.com>
2018-04-23 13:38:37 +0200

    More recipe and module migration (#57)
    * Migrate LocalFilesystem
    
    * Split GRR modules into their own files.
    
    * Migrate grr_artifact_hosts
    
    * Migrate grr_flow_download
    
    * migrate timesketch_upload
    
    * Cleanup
    
    * Bugfix
    
    * Update grr protos
    
    * migrate grr_hunt_artifacts
    
    * Migrate grr_hunt_file
    
    * Bugfix
    
    * huntresults_plaso_timesketch migration
    
    * Cleanup unused files
    
    * Cleanup docstrings and fix codacy errors.
    
    * Fix codacy error
    
    * Silence codacy
    
    * Remove unnecessary utils rename
    
    * Correct nit
    
    * Catch errors
    
    * Added missing parameter
    
    * Celanup nits

--- dftimewolf/cli/dftimewolf_recipes.py
+++ dftimewolf/cli/dftimewolf_recipes.py
@@ -10,22 +10,40 @@ import signal
 from dftimewolf import config
 
 from dftimewolf.cli.recipes import local_plaso
+from dftimewolf.cli.recipes import grr_artifact_hosts
+from dftimewolf.cli.recipes import grr_flow_download
+from dftimewolf.cli.recipes import grr_hunt_artifacts
+from dftimewolf.cli.recipes import grr_hunt_file
+from dftimewolf.cli.recipes import grr_huntresults_plaso_timesketch
+from dftimewolf.cli.recipes import timesketch_upload
 
-from dftimewolf.lib import utils as dftw_utils
+from dftimewolf.lib import utils
 
 from dftimewolf.lib.collectors import filesystem
+from dftimewolf.lib.collectors import grr_hosts
+from dftimewolf.lib.collectors import grr_hunt
 from dftimewolf.lib.exporters import timesketch
+from dftimewolf.lib.exporters import local_filesystem
 from dftimewolf.lib.processors import localplaso
 
 from dftimewolf.lib.state import DFTimewolfState
-from dftimewolf.lib.utils import DFTimewolfFormatterClass
 
 
-signal.signal(signal.SIGINT, dftw_utils.signal_handler)
+signal.signal(signal.SIGINT, utils.signal_handler)
 
 config.Config.register_module(filesystem.FilesystemCollector)
 config.Config.register_module(localplaso.LocalPlasoProcessor)
 config.Config.register_module(timesketch.TimesketchExporter)
+
+config.Config.register_module(grr_hosts.GRRArtifactCollector)
+config.Config.register_module(grr_hosts.GRRFileCollector)
+config.Config.register_module(grr_hosts.GRRFlowCollector)
+config.Config.register_module(grr_hunt.GRRHuntArtifactCollector)
+config.Config.register_module(grr_hunt.GRRHuntFileCollector)
+config.Config.register_module(grr_hunt.GRRHuntDownloader)
+
+config.Config.register_module(timesketch.TimesketchExporter)
+config.Config.register_module(local_filesystem.LocalFilesystemCopy)
 
 # Try to open config.json and load configuration data from it.
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
@@ -34,6 +52,12 @@ config.Config.load_extra(os.path.join(ROOT_DIR, 'config.json'))
 config.Config.load_extra(os.path.join(USER_DIR, '.dftimewolfrc'))
 
 config.Config.register_recipe(local_plaso)
+config.Config.register_recipe(grr_artifact_hosts)
+config.Config.register_recipe(grr_flow_download)
+config.Config.register_recipe(grr_hunt_artifacts)
+config.Config.register_recipe(grr_hunt_file)
+config.Config.register_recipe(grr_huntresults_plaso_timesketch)
+config.Config.register_recipe(timesketch_upload)
 
 # TODO(tomchop) Change the print statements by a better logging / display system
 
@@ -60,7 +84,7 @@ def main():
     recipe, recipe_args, documentation = registered_recipe
     subparser = subparsers.add_parser(
         recipe['name'],
-        formatter_class=DFTimewolfFormatterClass,
+        formatter_class=utils.DFTimewolfFormatterClass,
         description='{0:s}'.format(documentation))
     subparser.set_defaults(recipe=recipe)
     for switch, help_text, default in recipe_args:
@@ -72,16 +96,12 @@ def main():
   args = parser.parse_args()
   recipe = args.recipe
 
-  console_out = dftw_utils.DFTimewolfConsoleOutput(
-      sender='DFTimewolfCli', verbose=True)
-
   # Thread all collectors.
-  console_out.StdOut('Collectors:')
   state = DFTimewolfState()
 
   for module_description in recipe['modules']:
     # Combine CLI args with args from the recipe description
-    new_args = dftw_utils.import_args_from_dict(
+    new_args = utils.import_args_from_dict(
         module_description['args'], vars(args), config.Config)
 
     # Create the module object and start processing
@@ -89,6 +109,7 @@ def main():
     print 'Running module {0:s}'.format(module_name)
     module = config.Config.get_module(module_name)(state)
     module.setup(**new_args)
+    state.check_errors()
     module.process()
 
     # Check for eventual errors and clean up after each round.
--- dftimewolf/lib/exporters/local_filesystem.py
+++ dftimewolf/lib/exporters/local_filesystem.py
@@ -1,52 +1,48 @@
 # -*- coding: utf-8 -*-
-"""Export processing results to Timesketch."""
+"""Collect artifacts from the local filesystem."""
 
-from __future__ import print_function
 from __future__ import unicode_literals
 
 import os
 import shutil
 
-from dftimewolf.lib.exporters.exporters import BaseExporter
+from dftimewolf.lib.module import BaseModule
 
 
-class LocalFilesystemExporter(BaseExporter):
-  """Export results of a collector or processor to the local filesystem.
+class LocalFilesystemCopy(BaseModule):
+  """Copies the files in the previous module's output to a given path.
 
-  Attributes:
-    previous_output: (name, path) tuple to export.
-    directory: (str) absolute or relative path pointing to a directory into
-        which files should be copied.
+  input: List of paths to copy the files from.
+  output: The directory in which the files have been copied.
   """
 
-  def __init__(self, previous_output, directory, verbose=False):
-    """Initializes a LocalFilesystemExporter
+  def __init__(self, state):
+    super(LocalFilesystemCopy, self).__init__(state)
+    self._target_directory = None
+
+  def setup(self, target_directory=None): # pylint: disable=arguments-differ
+    """Sets up the _paths attribute
 
     Args:
-      previous_output: (name, path) tuple to export.
-      directory: directory into which files should be copied.
-      verbose: Whether verbose output is desired.
+      paths: Comma-separated list of strings represnting the paths to collect.
     """
-    super(LocalFilesystemExporter, self).__init__(verbose=verbose)
-    self.previous_output = previous_output
-    self.directory = directory
-    self.output = []
-
-    if not os.path.exists(directory):
+    if not os.path.exists(target_directory):
       try:
-        os.makedirs(directory)
+        os.makedirs(target_directory)
       except OSError as exception:
-        self.console_out.StdErr(
-            'An unknown error occurred: {0:s}'.format(exception))
-    self.console_out.VerboseOut(
-        'Files will be copied to {0:s}'.format(directory))
+        message = 'An unknown error occurred: {0:s}'.format(exception)
+        self.state.add_error(message, critical=True)
+    self._target_directory = target_directory
 
-  def export(self):
-    """Does the actual copying of files."""
-    for source, path in self.previous_output:
-      self._copy_file_or_directory(path, self.directory)
-      self.console_out.StdOut(
-          '({0:s}) {1:s} -> {2:s}'.format(source, path, self.directory))
+  def cleanup(self):
+    pass
+
+  def process(self):
+    """Checks whether the paths exists and updates the state accordingly."""
+    for host, path in self.state.input:
+      directory = os.path.join(self._target_directory, host)
+      self._copy_file_or_directory(path, directory)
+      print '{0:s} -> {1:s}'.format(path, directory)
 
   def _copy_file_or_directory(self, source, destination_directory):
     """Recursively copies files from source to destination_directory.
@@ -55,28 +51,11 @@ class LocalFilesystemExporter(BaseExporter):
         source: source file or directory to copy into destination_directory
         destination_directory: destination directory in which to copy source
     """
-    for item in os.listdir(source):
-      full_source = os.path.join(source, item)
-      full_destination = os.path.join(destination_directory, item)
-      if os.path.isdir(full_source):
+    if os.path.isdir(source):
+      for item in os.listdir(source):
+        full_source = os.path.join(source, item)
+        full_destination = os.path.join(destination_directory, item)
         shutil.copytree(full_source, full_destination)
-      else:
-        shutil.copy2(full_source, full_destination)
-
-  @staticmethod
-  def launch_exporter(processor_output, directory):
-    """Threads one or more LocalFilesystemExporter objects.
-
-    Args:
-      processor_output: List of (name, path) tuples to export.
-      directory: destination directory of copy.
-
-    Returns:
-      A list consisting of a single LocalFilesystemExporter object that can be
-          join()ed from the caller.
-    """
-    exporter = LocalFilesystemExporter(processor_output, directory)
-    exporter.start()
-    return [exporter]
-
-MODCLASS = [('timesketch', LocalFilesystemExporter)]
+    else:
+      print source, destination_directory
+      shutil.copy2(source, destination_directory)
