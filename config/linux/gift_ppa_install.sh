ac791eff424265cad2c62e4158519f3a4ca32893
Daniel White <onager@deerpie.com>
2018-05-08 15:30:53 +0200

    Add slightly more sanity to docker install command

--- config/linux/gift_ppa_install.sh
+++ config/linux/gift_ppa_install.sh
@@ -44,7 +44,8 @@ if [[ "$*" =~ "include-docker" ]]; then
        stable"
     sudo apt-get update -q
     sudo apt-get install -y docker-ce
-    sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+    curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m)
+    sudo cp docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
 fi
 
