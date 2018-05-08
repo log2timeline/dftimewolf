faf9a5550e42a2aa77645ecb4538214edca3ef68
Daniel White <onager@deerpie.com>
2018-05-08 14:38:26 +0200

    Revert change from other branch

--- config/linux/gift_ppa_install.sh
+++ config/linux/gift_ppa_install.sh
@@ -3,8 +3,7 @@ set -e
 
 # Dependencies for running dftimewolf, alphabetized, one per line.
 # This should not include packages only required for testing or development.
-PYTHON2_DEPENDENCIES="python-bs4
-                      python-certifi
+PYTHON2_DEPENDENCIES="python-certifi
                       python-chardet
                       python-idna
                       python-requests
@@ -24,46 +23,10 @@ sudo add-apt-repository ppa:gift/dev -y
 sudo apt-get update -q
 sudo apt-get install -y ${PYTHON2_DEPENDENCIES}
 
-# Pending resolution of https://github.com/log2timeline/l2tdevtools/issues/233.
-sudo apt-get install -y python-pip
-sudo pip install grr-api-client
-
 if [[ "$*" =~ "include-development" ]]; then
     sudo apt-get install -y ${DEVELOPMENT_DEPENDENCIES}
 fi
 
 if [[ "$*" =~ "include-test" ]]; then
     sudo apt-get install -y ${TEST_DEPENDENCIES}
-fi
-
-if [[ "$*" =~ "include-docker" ]]; then
-    # Install docker
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository \
-       "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-       $(lsb_release -cs) \
-       stable"
-    sudo apt-get update -q
-    sudo apt-get install -y docker-ce
-fi
-
-if [[ "$*" =~ "include-grr" ]]; then
-    # Start the GRR server container.
-    mkdir ~/grr-docker
-    sudo docker run \
-      --name grr-server -v ~/grr-docker/db:/var/grr-datastore \
-      -v ~/grr-docker/logs:/var/log \
-      -e EXTERNAL_HOSTNAME="localhost" \
-      -e ADMIN_PASSWORD="demo" \
-      --ulimit nofile=1048576:1048576 \
-      -p 0.0.0.0:8000:8000 -p 0.0.0.0:8080:8080 \
-      -d grrdocker/grr:latest grr
-
-    # Install the client.
-    sudo docker cp grr-server:/usr/share/grr-server/executables/installers .
-    sudo dpkg -i installers/*amd64.deb
-fi
-
-if [[ "$*" =~ "include-plaso" ]]; then
-    sudo apt-get -y install plaso-tools
 fi
