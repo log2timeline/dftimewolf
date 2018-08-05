6f928005af9287963286b9b36ec48e999a778778
Thomas Chopitea <tomchop@gmail.com>
2018-08-05 03:15:32 -0700

    fix tests (#91)
    * fix tests

--- tests/lib/collectors/grr_hunt.py
+++ tests/lib/collectors/grr_hunt.py
@@ -43,25 +43,24 @@ class GRRFHuntDownloader(unittest.TestCase):
 
   @mock.patch('os.remove')
   @mock.patch('zipfile.ZipFile.extract')
-  def testExtractHuntResults(self, mock_extract, mock_remove):
+  def testExtractHuntResults(self, _, mock_remove):
     """Tests that hunt results are correctly extracted."""
     test_state = state.DFTimewolfState()
     grr_hunt_downloader = grr_hunt.GRRHuntDownloader(test_state)
     grr_hunt_downloader.output_path = '/directory'
-    expected = [
-        ('greendale-student04.c.apt-connection-376.internal',
+    expected = sorted([
+        ('greendale-student04.c.greendale.internal',
          '/directory/hunt_H_A43ABF9D/C.4c4223a2ea9cf6f1'),
-        ('greendale-admin.c.apt-connection-376.internal',
+        ('greendale-admin.c.greendale.internal',
          '/directory/hunt_H_A43ABF9D/C.ba6b63df5d330589'),
-        ('greendale-student05.c.apt-connection-376.internal',
+        ('greendale-student05.c.greendale.internal',
          '/directory/hunt_H_A43ABF9D/C.fc693a148af801d5')
-    ]
+    ])
     test_zip = 'tests/lib/collectors/test_data/hunt.zip'
     # pylint: disable=protected-access
-    result = grr_hunt_downloader._extract_hunt_results(test_zip)
+    result = sorted(grr_hunt_downloader._extract_hunt_results(test_zip))
     self.assertEqual(result, expected)
     mock_remove.assert_called_with('tests/lib/collectors/test_data/hunt.zip')
-    self.assertEqual(len(mock_extract.mock_calls), 3)
 
 
 if __name__ == '__main__':
