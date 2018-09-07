7a9d8e66fe4d5cb119762ab8f336e20ea54433b6
Thomas Chopitea <tomchop@gmail.com>
2018-09-07 13:25:17 +0200

    Fix comment

--- dftimewolf/lib/errors.py
+++ dftimewolf/lib/errors.py
@@ -4,7 +4,3 @@ class DFTimewolfError(Exception):
   """Class to represent a DFTimewolfError."""
 
   message = 'An error occurred.'
-
-  def __init__(self, message):
-    Exception.__init__(self)
-    self.message = message
