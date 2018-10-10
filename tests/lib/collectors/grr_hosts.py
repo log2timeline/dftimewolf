ec36a2002fb13f1907f7f853894e5003aa2db066
Thomas Chopitea <tomchop@gmail.com>
2018-10-10 11:28:36 +0200

    Fix test

--- tests/lib/collectors/grr_hosts.py
+++ tests/lib/collectors/grr_hosts.py
@@ -188,6 +188,7 @@ class GRRArtifactCollectorTest(unittest.TestCase):
                      ['tomchop', 'tomchop2'])
     self.assertTrue(self.grr_artifact_collector.use_tsk)
 
+  @mock.patch('grr_api_client.flow.FlowBase.Get')
   @mock.patch('grr_api_client.client.ClientBase.CreateFlow')
   @mock.patch('dftimewolf.lib.collectors.grr_hosts.GRRFlow._download_files')
   @mock.patch('grr_response_proto.flows_pb2.ArtifactCollectorFlowArgs')
@@ -196,11 +197,13 @@ class GRRArtifactCollectorTest(unittest.TestCase):
                                    mock_SearchClients,
                                    mock_ArtifactCollectorFlowArgs,
                                    mock_DownloadFiles,
-                                   mock_CreateFlow):
+                                   mock_CreateFlow,
+                                   mock_Get):
     """Tests that artifacts defined during setup are searched for."""
     mock_DownloadFiles.return_value = '/tmp/tmpRandom/tomchop'
     mock_SearchClients.return_value = mock_grr_hosts.MOCK_CLIENT_LIST
     mock_CreateFlow.return_value = mock_grr_hosts.MOCK_FLOW
+    mock_Get.return_value = mock_grr_hosts.MOCK_FLOW
     self.grr_artifact_collector = grr_hosts.GRRArtifactCollector(
         self.test_state)
     self.grr_artifact_collector.setup(
