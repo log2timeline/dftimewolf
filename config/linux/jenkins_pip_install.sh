951b0666454ecc2ce46f92d9d8cdcfe54d635d64
Thomas Chopitea <tomchop@gmail.com>
2019-02-21 17:27:56 +0100

    Move and self-contain install script

--- config/linux/jenkins_pip_install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#
-# This file was manually written to ensure proper dependencies for dftimewolf
-# are installed pending packaging in GIFT.
-
-pip install protobuf==3.6.1
-pip install -r requirements.txt
