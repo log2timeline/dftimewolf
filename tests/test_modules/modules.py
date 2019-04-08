02b757b778fbeab4d769c0fa22e726008634a339
Thomas Chopitea <tomchop@gmail.com>
2019-04-08 16:44:37 +1000

    Fix linter

--- tests/test_modules/modules.py
+++ tests/test_modules/modules.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Contains dummy modules used in tests."""
 
-from __future__ import unicode_literals
+from __future__ import print_function, unicode_literals
 
 from dftimewolf.lib.module import BaseModule
 
