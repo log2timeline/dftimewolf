56bc675401367048bd8dd3af68c8d16b91cab8a3
Joachim Metz <joachim.metz@gmail.com>
2019-06-06 15:14:56 +0200

    Added recipe class #161 (#164)

---
+++ tests/lib/resources.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Tests for the various dfTimewolf resource objects."""
+
+from __future__ import unicode_literals
+
+import unittest
+
+from dftimewolf.lib import resources
+
+
+class RecipeTest(unittest.TestCase):
+  """Tests Recipe."""
+
+  _ARGS = [
+      ('test', 'Test argument', None),
+  ]
+
+  _CONTENTS = {
+      'name': 'test',
+      'short_description': 'recipe description',
+      'modules': [{
+          'wants': [],
+          'name': 'TestModule',
+          'args': {
+              'test': '@test'
+          },
+      }],
+  }
+
+  _DESCRIPTION = 'test recipe'
+
+  def testInitialize(self):
+    """Tests the __init__ function."""
+    recipe = resources.Recipe(self._DESCRIPTION, self._CONTENTS, self._ARGS)
+    self.assertIsNotNone(recipe)
+
+  def testGetHelpString(self):
+    """Tests the GetHelpString function."""
+    recipe = resources.Recipe(self._DESCRIPTION, self._CONTENTS, self._ARGS)
+
+    expected_help_string = (
+        ' test                               recipe description\n')
+    help_string = recipe.GetHelpString()
+    self.assertEqual(help_string, expected_help_string)
+
+
+if __name__ == '__main__':
+  unittest.main()
