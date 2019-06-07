f0de1ef29da7f97561f30d65ae5286a2a2919f19
Floris Westermann <westermann@google.com>
2019-06-07 12:20:53 +0200

    /dftimewolf/lib/exporters: Add new exporter scp_ex
    Create new exporter and add a simple example recipe.

---
+++ dftimewolf/lib/exporters/scp_ex.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""Collect artifacts from the local filesystem."""
+
+from __future__ import unicode_literals
+
+import subprocess
+import getpass
+
+from dftimewolf.lib.module import BaseModule
+
+class Scp(BaseModule):
+  """Copies the files in the previous module's output to a given path.
+
+  input: List of paths to copy the files from.
+  output: The directory in which the files have been copied.
+  """
+
+  def __init__(self, state):
+    super(Scp, self).__init__(state)
+    self._target_directory = None
+
+  def setup(self, paths, destination, user=None, hostname=None, id_file=None):
+    """Sets up the _target_directory attribute.
+
+    Args:
+      target_directory: Directory in which collected files will be dumped.
+      paths: List of files to copy.
+      user: Username at destination host.
+      hostname: Hostname of destination.
+      destination: Path to destination on host.
+      id_file: Identity file to use.
+    """
+    self._paths = paths.split(",")
+    self._user = user if user else getpass.getuser()
+    self._hostname = hostname
+    self._destination = destination
+    self._id_file = id_file
+
+    if not self._ssh_available():
+      self.state.add_error("Unable to connect to host.", critical=True)
+
+  def cleanup(self):
+    pass
+
+  def process(self):
+    """copies the list of paths to the destination on user@hostname"""
+    if self._hostname:
+      dest = "{0:s}@{1:s}:{2:s}".format(self._user, self._hostname, self._destination)
+    dest = self._destination
+
+    cmd = ["scp"]
+    cmd.extend(self._paths)
+    cmd.append(dest)
+    if subprocess.call(cmd):
+      self.state.add_error("Failed copying {0:s}".format(self._paths), critical=False)
+
+  def _ssh_available(self):
+    """returns true if host can be reached, false otherwise"""
+    if not self._hostname:
+      return True
+
+    command = ["ssh", "-q", self._hostname]
+    if self._id_file:
+      command.extend(["-i", self._id_file])
+    return not subprocess.call(command)
