71cbba8e591f92b066b944e1a93f1bf36d236e35
Thomas Chopitea <tomchop@gmail.com>
2019-06-07 14:50:48 +0200

    Fix indentation

--- dftimewolf/cli/dftimewolf_recipes.py
+++ dftimewolf/cli/dftimewolf_recipes.py
@@ -90,10 +90,9 @@ def check_python_version():
   """Checks that we're running a compatible version of Python."""
   version_tuple = (sys.version_info[0], sys.version_info[1])
   if version_tuple[0] != 3 or version_tuple < (3, 6):
-  print((
-      'Unsupported Python version: {0:s}, version 3.6 or higher '
-      'required.').format(sys.version))
-  sys.exit(1)
+    print(('Unsupported Python version: {0:s}, version 3.6 or higher '
+           'required.').format(sys.version))
+    sys.exit(1)
 
 
 def main():
