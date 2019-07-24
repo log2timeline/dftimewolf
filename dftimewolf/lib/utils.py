f039c4595477e8cb9efcf0237774c9bb4397a5b7
Joachim Metz <joachim.metz@gmail.com>
2019-07-24 06:42:14 +0200

    cstyle7

--- dftimewolf/lib/utils.py
+++ dftimewolf/lib/utils.py
@@ -5,11 +5,12 @@ from __future__ import unicode_literals
 
 import argparse
 import re
-import sys
 
 import six
 
+
 TOKEN_REGEX = re.compile(r'\@([\w_]+)')
+
 
 # preserve python2 compatibility
 # pylint: disable=unnecessary-pass
@@ -20,7 +21,7 @@ class DFTimewolfFormatterClass(
   pass
 
 
-def import_args_from_dict(value, args, config):
+def ImportArgsFromDict(value, args, config):
   """Replaces some arguments by those specified by a key-value dictionary.
 
   This function will be recursively called on a dictionary looking for any
@@ -31,15 +32,15 @@ def import_args_from_dict(value, args, config):
   parameters passed in recipes.
 
   Args:
-    value: The value of a {key: value} dictionary. This is passed recursively
-        and may change in nature: string, list, or dict. The top-level variable
-        should be the dictionary that is supposed to be recursively traversed.
-    args: A {key: value} dictionary used to do replacements.
-    config: A dftimewolf.Config class containing configuration information
+    value (object): The value a dictionary. This is passed recursively and may
+        change in nature: string, list, or dict. The top-level variable should
+        be the dictionary that is supposed to be recursively traversed.
+    args (dict[str, object]): dictionary used to do replacements.
+    config (dftimewolf.Config): class containing configuration information
 
   Returns:
-    The first caller of the function will receive a dictionary in which strings
-    starting with "@" are replaced by the parameters in args.
+    object: the first caller of the function will receive a dictionary in
+        which strings starting with "@" are replaced by the parameters in args.
   """
   if isinstance(value, six.string_types):
     for match in TOKEN_REGEX.finditer(str(value)):
@@ -51,43 +52,12 @@ def import_args_from_dict(value, args, config):
         else:
           value = actual_param
   elif isinstance(value, list):
-    return [import_args_from_dict(item, args, config) for item in value]
+    return [ImportArgsFromDict(item, args, config) for item in value]
   elif isinstance(value, dict):
     return {
-        key: import_args_from_dict(val, args, config)
+        key: ImportArgsFromDict(val, args, config)
         for key, val in value.items()
     }
   elif isinstance(value, tuple):
-    return tuple(import_args_from_dict(val, args, config) for val in value)
+    return tuple(ImportArgsFromDict(val, args, config) for val in value)
   return value
-
-
-def check_placeholders(value):
-  """Checks if any values in a given dictionary still contain @ parameters.
-
-  Args:
-    value: Dictionary, list, or string that will be recursively checked for
-        placeholders
-
-  Raises:
-    ValueError: There still exists a value with an @ parameter.
-
-  Returns:
-    Top-level caller: a modified dict with replaced tokens.
-    Recursive caller: a modified object with replaced tokens.
-  """
-  if isinstance(value, six.string_types):
-    if TOKEN_REGEX.search(value):
-      raise ValueError('{0:s} must be replaced in dictionary'.format(value))
-  elif isinstance(value, list):
-    return [check_placeholders(item) for item in value]
-  elif isinstance(value, dict):
-    return {key: check_placeholders(val) for key, val in value.items()}
-  elif isinstance(value, tuple):
-    return tuple(check_placeholders(val) for val in value)
-  return value
-
-def signal_handler(*unused_argvs):
-  """Catches Ctrl + C to exit cleanly."""
-  sys.stderr.write("\nCtrl^C caught, bailing...\n")
-  sys.exit(0)
