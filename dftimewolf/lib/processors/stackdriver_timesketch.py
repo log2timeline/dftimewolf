21188eea30714448da62c4951654f99ce5822836
Daniel White <onager@deerpie.com>
2019-08-12 14:17:17 +0200

    Added processor for making Stackdriver logs readable in Timesketch (#180)
    * Added processor for making Stackdriver logs readable in Timesketch
    
    * Changes after review
    
    * Changes after rebase
    
    * Fixes after rebase

---
+++ dftimewolf/lib/processors/stackdriver_timesketch.py
@@ -0,0 +1,230 @@
+# -*- coding: utf-8 -*-
+"""Processes Stackdriver logs for loading into Timesketch."""
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import tempfile
+import json
+
+from dftimewolf.lib.module import BaseModule
+from dftimewolf.lib.containers import StackdriverLogs
+
+
+class StackdriverTimesketch(BaseModule):
+  """Transforms Stackdriver logs for Timesketch."""
+
+  def SetUp(self, *args, **kwargs):
+    """Sets up necessary module configuration options."""
+    # No configuration required.
+
+  def __init__(self, state):
+    super(StackdriverTimesketch, self).__init__(state)
+
+  def _ProcessLogLine(self, log_line, query, project_name):
+    """Processes a single JSON formatted Stackdriver log line.
+
+    Args:
+      log_line (str): a JSON formatted Stackdriver log entry.
+      query (str): the Stackdriver query used to retrieve the log.
+      project_name (str): name of the GCP project associated with the query.
+
+    Returns:
+      str: a Timesketch-friendly version of the log line.
+    """
+    log_record = json.loads(log_line)
+
+    # Metadata about how the record was obtained.
+    timesketch_record = {'query': query, 'project_name': project_name}
+
+    # Timestamp related fields.
+    timestamp = log_record.get('timestamp', None)
+    if timestamp:
+      timesketch_record['datetime'] = timestamp
+      timesketch_record['timestamp_desc'] = 'Event Recorded'
+
+    # General resource information.
+    resource = log_record.get('resource', None)
+    if resource:
+      labels = resource.get('labels', None)
+      if labels:
+        for attribute, value in labels.items():
+          timesketch_attribute = 'resource_label_{0:s}'.format(attribute)
+          timesketch_record[timesketch_attribute] = value
+
+    # The log entry will have either a jsonPayload, a protoPayload or a
+    # textPayload.
+    json_payload = log_record.get('jsonPayload', None)
+    if json_payload:
+      self._ParseJSONPayload(json_payload, timesketch_record)
+
+    proto_payload = log_record.get('protoPayload', None)
+    if proto_payload:
+      self._parse_proto_payload(proto_payload, timesketch_record)
+
+    text_payload = log_record.get('textPayload', None)
+    if text_payload:
+      timesketch_record['textPayload'] = text_payload
+
+    self._BuildMessageString(timesketch_record)
+
+    return json.dumps(timesketch_record)
+
+  def _parse_proto_payload(self, proto_payload, timesketch_record):
+    """Extracts information from a protoPayload field in a Stackdriver log.
+
+    protoPayload is set for all cloud audit events.
+
+    Args:
+      proto_payload (dict): the contents of a Stackdriver protoPayload field.
+      timesketch_record (dict): a dictionary that will be serialized to JSON
+        and uploaded to Timesketch.
+    """
+    authentication_info = proto_payload.get('authenticationInfo', None)
+    if authentication_info:
+      principal_email = authentication_info.get('principalEmail', None)
+      if principal_email:
+        timesketch_record['principalEmail'] = principal_email
+
+    request_metadata = proto_payload.get('requestMetadata', None)
+    if request_metadata:
+      for attribute, value in request_metadata.items():
+        timesketch_attribute = 'requestMetadata_{0:s}'.format(attribute)
+        timesketch_record[timesketch_attribute] = value
+
+    proto_attributes = ['serviceName', 'methodName', 'resourceName']
+    for attribute in proto_attributes:
+      value = proto_payload.get(attribute, None)
+      if value:
+        timesketch_record[attribute] = value
+
+    request = proto_payload.get('request', None)
+    if request:
+      self._ParseProtoPayloadRequest(request, timesketch_record)
+
+  def _ParseProtoPayloadRequest(self, request, timesketch_record):
+    """Extracts information from the request field of a protoPayload field.
+
+    Args:
+      request (dict): the contents of a Stackdriver request field from a
+          protoPayload field.
+      timesketch_record (dict): a dictionary that will be serialized to JSON
+        and uploaded to Timesketch.
+    """
+    request_attributes = [
+        'name', 'description', 'direction', 'member', 'targetTags', 'email',
+        'account_id'
+    ]
+    for attribute in request_attributes:
+      if attribute in request:
+        timesketch_attribute = 'request_{0:s}'.format(attribute)
+        timesketch_record[timesketch_attribute] = request[attribute]
+
+    # Firewall specific attributes.
+    if 'sourceRanges' in request:
+      source_ranges = ', '.join(request['sourceRanges'])
+      timesketch_record['source_ranges'] = source_ranges
+
+    if 'alloweds' in request:
+      for allowed in request['alloweds']:
+        attribute_name = 'allowed_{0:s}_ports'.format(allowed['IPProtocol'])
+        if 'ports' in allowed:
+          timesketch_record[attribute_name] = allowed['ports']
+        else:
+          timesketch_record[attribute_name] = 'all'
+
+    if 'denieds' in request:
+      for denied in request['denieds']:
+        attribute_name = 'denied_{0:s}_ports'.format(denied['IPProtocol'])
+        if 'ports' in denied:
+          timesketch_record[attribute_name] = denied['ports']
+        else:
+          timesketch_record[attribute_name] = 'all'
+
+    # Service account specific attributes
+    if 'service_account' in request:
+      service_account_name = request['service_account'].get('display_name')
+      timesketch_record['service_account_display_name'] = service_account_name
+
+  def _ParseJSONPayload(self, json_payload, timesketch_record):
+    """Extracts information from a json_payload.
+
+    Args:
+      json_payload (dict): the contents of a Stackdriver jsonPayload field.
+      timesketch_record (dict): a dictionary that will be serialized to JSON
+        and uploaded to Timesketch.
+    """
+    json_attributes = ['event_type', 'event_subtype']
+    for attribute in json_attributes:
+      if attribute in json_payload:
+        timesketch_record[attribute] = json_payload[attribute]
+
+    actor = json_payload.get('actor', None)
+    if actor:
+      if 'user' in actor:
+        timesketch_record['user'] = actor['user']
+
+  def _BuildMessageString(self, timesketch_record):
+    """Builds a Timesketch message string from a Timesketch record.
+
+    Args:
+      timesketch_record (dict): a dictionary that will be serialized to JSON
+        and uploaded to Timesketch.
+    """
+    user = ''
+    action = ''
+    resource = ''
+
+    # Ordered from least to most preferred value
+    user_attributes = ['principalEmail', 'user']
+    for attribute in user_attributes:
+      if attribute in timesketch_record:
+        user = timesketch_record[attribute]
+
+    # Ordered from least to most preferred value
+    action_attributes = ['methodName', 'event_subtype']
+    for attribute in action_attributes:
+      if attribute in timesketch_record:
+        action = timesketch_record[attribute]
+
+    # Ordered from least to most preferred value
+    resource_attributes = ['resource_label_instance_id', 'resourceName']
+    for attribute in resource_attributes:
+      if attribute in timesketch_record:
+        resource = timesketch_record[attribute]
+
+    message = 'User {0:s} performed {1:s} on {2:s}'.format(
+        user, action, resource)
+    timesketch_record['message'] = message
+
+  def _ProcessLogContainer(self, logs_container):
+    """Processes a Stackdriver logs container.
+
+    Args:
+      logs_container (StackdriverLogs): logs container.
+    """
+    if not logs_container.path:
+      return
+
+    output_file = tempfile.NamedTemporaryFile(
+        mode='w', encoding='utf-8', delete=False)
+    output_path = output_file.name
+
+    with open(logs_container.path, 'r') as input_file:
+      for line in input_file:
+        transformed_line = self._ProcessLogLine(
+            line, logs_container.filter_expression, logs_container.project_name)
+        if transformed_line:
+          output_file.write(transformed_line)
+          output_file.write('\n')
+    output_file.close()
+
+    timeline_name = 'Stackdriver logs {0:s} "{1:s}"'.format(
+        logs_container.project_name, logs_container.filter_expression)
+
+    self.state.input.append([timeline_name, output_path])
+
+  def Process(self):
+    """Processes Stackdriver logs containers for insertion into Timesketch."""
+    logs_containers = self.state.get_containers(StackdriverLogs)
+    for logs_container in logs_containers:
+      self._ProcessLogContainer(logs_container)
---
+++ tests/lib/processors/stackdriver_timesketch.py
@@ -0,0 +1,342 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''Tests the stackdriver timesketch processor.'''
+
+from __future__ import unicode_literals
+
+import json
+import unittest
+
+from dftimewolf.lib import state
+from dftimewolf.lib.processors import stackdriver_timesketch
+
+from dftimewolf import config
+
+
+class StackdriverTimesketchTest(unittest.TestCase):
+  """Tests for the Stackdriver Timesketch processor."""
+
+  def testInitialization(self):
+    """Tests that the processor can be initialized."""
+    test_state = state.DFTimewolfState(config.Config)
+    processor = stackdriver_timesketch.StackdriverTimesketch(test_state)
+    self.assertIsNotNone(processor)
+
+  def testGCEFirewallLog(self):
+    """Tests that a firewall log is transformed correctly."""
+    test_state = state.DFTimewolfState(config.Config)
+    processor = stackdriver_timesketch.StackdriverTimesketch(test_state)
+
+    firewall_addition = {
+        'logName':
+            'projects/ketchup-research/logs/'
+            'cloudaudit.googleapis.com%2Factivity',
+        'resource': {
+            'type': 'gce_firewall_rule',
+            'labels': {
+                'firewall_rule_id': '2527368186053355716',
+                'project_id': 'ketchup-research'
+            }
+        },
+        'insertId':
+            '9g6l0dd4nlo',
+        'severity':
+            'NOTICE',
+        'timestamp':
+            '2019-06-06T09:00:41.797000Z',
+        'operation': {
+            'id': 'operation-1559811626986-58aa3f1f16a4c-6840f50a-8fab23b9',
+            'producer': 'compute.googleapis.com',
+            'last': True
+        },
+        'protoPayload': {
+            '@type':
+                'type.googleapis.com/google.cloud.audit.AuditLog',
+            'authenticationInfo': {
+                'principalEmail':
+                    'heinz-57@ketchup-research.iam.gserviceaccount.com'
+            },
+            'requestMetadata': {
+                'callerIp': 'gce-internal-ip',
+                'callerSuppliedUserAgent': 'google-cloud-sdk gcloud/249.0.0'
+            },
+            'serviceName':
+                'compute.googleapis.com',
+            'methodName':
+                'v1.compute.firewalls.insert',
+            'resourceName':
+                'projects/ketchup-research/global/firewalls/'
+                'deny-tomchop-access',
+            'request': {
+                '@type': 'type.googleapis.com/compute.firewalls.insert'
+            }
+        }
+    }
+    firewall_addition_json = json.dumps(firewall_addition)
+
+    expected_addition_record = {
+        'query':
+            'test_query',
+        'project_name':
+            'test_project',
+        'datetime':
+            '2019-06-06T09:00:41.797000Z',
+        'timestamp_desc':
+            'Event Recorded',
+        'resource_label_firewall_rule_id':
+            '2527368186053355716',
+        'resource_label_project_id':
+            'ketchup-research',
+        'principalEmail':
+            'heinz-57@ketchup-research.iam.gserviceaccount.com',
+        'requestMetadata_callerIp':
+            'gce-internal-ip',
+        'requestMetadata_callerSuppliedUserAgent':
+            'google-cloud-sdk gcloud/249.0.0',
+        'serviceName':
+            'compute.googleapis.com',
+        'methodName':
+            'v1.compute.firewalls.insert',
+        'resourceName':
+            'projects/ketchup-research/global/firewalls/deny-tomchop-access',
+        'message':
+            'User heinz-57@ketchup-research.iam.gserviceaccount.com '
+            'performed v1.compute.firewalls.insert '
+            'on projects/ketchup-research/global/firewalls/deny-tomchop-access'
+    }
+
+    # pylint: disable=protected-access
+    actual_timesketch_record = processor._ProcessLogLine(
+        firewall_addition_json, 'test_query', 'test_project')
+    actual_timesketch_record = json.loads(actual_timesketch_record)
+    self.assertDictEqual(expected_addition_record, actual_timesketch_record)
+
+    firewall_creation = {
+        'logName':
+            'projects/ketchup-research/logs/'
+            'cloudaudit.googleapis.com%2Factivity',
+        'resource': {
+            'type': 'gce_firewall_rule',
+            'labels': {
+                'firewall_rule_id': '2527368186053355716',
+                'project_id': 'ketchup-research'
+            }
+        },
+        'insertId':
+            '329e1bd92jo',
+        'severity':
+            'NOTICE',
+        'timestamp':
+            '2019-06-06T09:00:27.066000Z',
+        'operation': {
+            'id': 'operation-1559811626986-58aa3f1f16a4c-6840f50a-8fab23b9',
+            'producer': 'type.googleapis.com',
+            'first': True
+        },
+        'protoPayload': {
+            '@type':
+                'type.googleapis.com/google.cloud.audit.AuditLog',
+            'authenticationInfo': {
+                'principalEmail':
+                    'heinz-57@ketchup-research.iam.gserviceaccount.com'
+            },
+            'requestMetadata': {
+                'callerIp': 'gce-internal-ip',
+                'callerSuppliedUserAgent': 'google-cloud-sdk gcloud/249.0.0'
+            },
+            'serviceName':
+                'compute.googleapis.com',
+            'methodName':
+                'v1.compute.firewalls.insert',
+            'authorizationInfo': [{
+                'permission': 'compute.firewalls.create',
+                'granted': True
+            }, {
+                'permission': 'compute.networks.updatePolicy',
+                'granted': True
+            }],
+            'resourceName':
+                'projects/ketchup-research/global/firewalls/'
+                'deny-tomchop-access',
+            'request': {
+                '@type': 'type.googleapis.com/compute.firewalls.insert',
+                'targetTags': ['webserver'],
+                'network':
+                    'https://www.googleapis.com/compute/v1/projects/'
+                    'ketchup-research/global/networks/ketchup-exfil',
+                'denieds': [{
+                    'IPProtocol': 'tcp'
+                }],
+                'name': 'deny-tomchop-access',
+                'direction': 'INGRESS',
+                'priority': '1000',
+                'sourceRanges': ['0.0.0.0/0']
+            },
+            'response': {
+                'insertTime':
+                    '2019-06-06T02:00:28.013-07:00',
+                'startTime':
+                    '2019-06-06T02:00:28.029-07:00',
+                'targetLink':
+                    'https://www.googleapis.com/compute/v1/projects'
+                    '/ketchup-research/global/firewalls/deny-tomchop'
+                    '-access',
+                'progress':
+                    '0',
+                'user':
+                    'heinz-57@ketchup-research.iam.gserviceaccount.com',
+                '@type':
+                    'type.googleapis.com/operation',
+                'selfLinkWithId':
+                    'https://www.googleapis.com/compute/v1/projects/'
+                    'ketchup-research/global/operations/7407667996112808132',
+                'operationType':
+                    'insert',
+                'status':
+                    'RUNNING',
+                'id':
+                    '7407667996112808132',
+                'targetId':
+                    '2527368186053355716',
+                'selfLink':
+                    'https://www.googleapis.com/compute/v1/removed',
+                'name':
+                    'operation-1559811626986-58aa3f1f16a4c-6840f50a-8fab23b9'
+            }
+        }
+    }
+
+    firewall_creation_json = json.dumps(firewall_creation)
+
+    expected_creation_record = {
+        'query':
+            'test_query',
+        'project_name':
+            'test_project',
+        'datetime':
+            '2019-06-06T09:00:27.066000Z',
+        'timestamp_desc':
+            'Event Recorded',
+        'resource_label_firewall_rule_id':
+            '2527368186053355716',
+        'resource_label_project_id':
+            'ketchup-research',
+        'principalEmail':
+            'heinz-57@ketchup-research.iam.gserviceaccount.com',
+        'requestMetadata_callerIp':
+            'gce-internal-ip',
+        'requestMetadata_callerSuppliedUserAgent':
+            'google-cloud-sdk gcloud/249.0.0',
+        'serviceName':
+            'compute.googleapis.com',
+        'methodName':
+            'v1.compute.firewalls.insert',
+        'resourceName':
+            'projects/ketchup-research/global/firewalls/deny-tomchop-access',
+        'request_name':
+            'deny-tomchop-access',
+        'request_direction':
+            'INGRESS',
+        'request_targetTags': ['webserver'],
+        'source_ranges':
+            '0.0.0.0/0',
+        'denied_tcp_ports':
+            'all',
+        'message':
+            'User heinz-57@ketchup-research.iam.gserviceaccount.com '
+            'performed v1.compute.firewalls.insert on '
+            'projects/ketchup-research/global/firewalls/deny-tomchop-access'
+    }
+
+    actual_timesketch_record = processor._ProcessLogLine(
+        firewall_creation_json, 'test_query', 'test_project')
+    actual_timesketch_record = json.loads(actual_timesketch_record)
+    self.assertDictEqual(expected_creation_record, actual_timesketch_record)
+
+  def testGCECreateLog(self):
+    """Tests that a GCE instance creation log is transformed correctly."""
+    test_state = state.DFTimewolfState(config.Config)
+    processor = stackdriver_timesketch.StackdriverTimesketch(test_state)
+
+    gce_creation = {
+        'logName':
+            'projects/ketchup-research/logs/'
+            'cloudaudit.googleapis.com%2Factivity',
+        'resource': {
+            'type': 'gce_instance',
+            'labels': {
+                'zone': 'europe-west1-c',
+                'project_id': 'ketchup-research',
+                'instance_id': '6662286141402997301'
+            }
+        },
+        'insertId': '-3vn1nsd4wgu',
+        'severity': 'NOTICE',
+        'timestamp': '2019-06-06T09:29:04.499000Z',
+        'operation': {
+            'id': 'operation-1559813338033-58aa457edecaa-437db0e5-08683c6e',
+            'producer': 'compute.googleapis.com',
+            'last': True
+        },
+        'protoPayload': {
+            '@type': 'type.googleapis.com/google.cloud.audit.AuditLog',
+            'authenticationInfo': {
+                'principalEmail':
+                    'heinz-57@ketchup-research.iam.gserviceaccount.com'
+            },
+            'requestMetadata': {
+                'callerIp': 'gce-internal-ip',
+                'callerSuppliedUserAgent': 'google-cloud-sdk gcloud/249.0.0'
+            },
+            'serviceName': 'compute.googleapis.com',
+            'methodName': 'v1.compute.instances.insert',
+            'resourceName':
+                'projects/ketchup-research/zones/europe-west1-c/instances/'
+                'example-instance-2',
+            'request': {
+                '@type': 'type.googleapis.com/compute.instances.insert'
+            }
+        }
+    }
+    gce_creation = json.dumps(gce_creation)
+
+    expected_timesketch_record = {
+        'query':
+            'test_query',
+        'project_name':
+            'test_project',
+        'datetime':
+            '2019-06-06T09:29:04.499000Z',
+        'timestamp_desc':
+            'Event Recorded',
+        'resource_label_zone':
+            'europe-west1-c',
+        'resource_label_project_id':
+            'ketchup-research',
+        'resource_label_instance_id':
+            '6662286141402997301',
+        'principalEmail':
+            'heinz-57@ketchup-research.iam.gserviceaccount.com',
+        'requestMetadata_callerIp':
+            'gce-internal-ip',
+        'requestMetadata_callerSuppliedUserAgent':
+            'google-cloud-sdk gcloud/249.0.0',
+        'serviceName':
+            'compute.googleapis.com',
+        'methodName':
+            'v1.compute.instances.insert',
+        'resourceName':
+            'projects/ketchup-research/zones/europe-west1-c/instances/'
+            'example-instance-2',
+        'message':
+            'User heinz-57@ketchup-research.iam.gserviceaccount.com '
+            'performed v1.compute.instances.insert '
+            'on projects/ketchup-research/zones/europe-west1-c/instances/'
+            'example-instance-2'
+    }
+
+    # pylint: disable=protected-access
+    actual_timesketch_record = processor._ProcessLogLine(
+        gce_creation, 'test_query', 'test_project')
+    actual_timesketch_record = json.loads(actual_timesketch_record)
+    self.assertDictEqual(expected_timesketch_record, actual_timesketch_record)
