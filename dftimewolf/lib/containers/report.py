102ba01764c2c624455cf1d9577be9a2cf4f3e7c
Aaron Peterson <aaronp@gmail.com>
2019-08-21 17:13:22 -0700

    Use Turbinia reports and version bump (#206)
    * Add text_format to Report object and bump version
    
    * Use new reporting

--- dftimewolf/lib/containers/report.py
+++ dftimewolf/lib/containers/report.py
@@ -5,26 +5,34 @@ from dftimewolf.lib.containers import interface
 
 class Report(interface.AttributeContainer):
   """Analysis report attribute container.
+
   Attributes:
     module_name (str): name of the module that generated the report.
     text (str): report text.
+    text_format (str): format of text in the report. Must be either 'plaintext'
+      or 'markdown'
     attributes (list): attribute list, dicts must contain 'name',
       'type', 'values' keys.
   """
   CONTAINER_TYPE = 'report'
 
-  def __init__(self, module_name, text, attributes=None):
+  def __init__(
+      self, module_name, text, text_format='plaintext', attributes=None):
     """Initializes the analysis report.
+
     Args:
       module_name (str): name of the analysis plugin that generated
           the report.
       text (str): report text.
+      text_format (str): format of text in the report. Must be either
+        'plaintext' or 'markdown'.
       attributes (list): attribute list of dicts that must contain 'name',
         'type', 'values' keys.
     """
     super(Report, self).__init__()
     self.module_name = module_name
     self.text = text
+    self.text_format = text_format
     if attributes is None:
       self.attributes = []
     else:
