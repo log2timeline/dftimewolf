2f92e332db69da70ec3203eddf3b4031670e413b
Thomas Chopitea <tomchop@gmail.com>
2019-08-30 15:31:09 +0200

    Move attribute containers to a single module

--- tests/lib/containers/containers.py
+++ tests/lib/containers/containers.py
@@ -1,13 +1,28 @@
 # -*- coding: utf-8 -*-
-"""Tests for the ThreatIntelligence attribute containers."""
+"""Tests for the Report attribute containers."""
 
 from __future__ import unicode_literals
 
 import unittest
 
-from dftimewolf.lib import containers
+from dftimewolf.lib.containers import containers
 
 class ReportDataTest(unittest.TestCase):
+  """Tests for the Report data attribute container."""
+
+  def testGetAttributeNames(self):
+    """Tests the GetAttributeNames function."""
+    attribute_container = containers.Report(module_name='name', text='text')
+
+    expected_attribute_names = [
+        'attributes', 'module_name', 'text', 'text_format']
+
+    attribute_names = sorted(attribute_container.GetAttributeNames())
+
+    self.assertEqual(attribute_names, expected_attribute_names)
+
+
+class ThreatIntelligenceDataTest(unittest.TestCase):
   """Tests for the Report data attribute container."""
 
   def testGetAttributeNames(self):
