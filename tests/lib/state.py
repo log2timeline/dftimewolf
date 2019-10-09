06ec176ab1efa5049eaf29accd0781ac71e9a77f
Daniel White <onager@deerpie.com>
2019-10-09 16:38:56 -0700

    Fix for recipes missing modules or preflights (#226)
    * Fix for recipes missing modules or preflights
    
    * Fix for recipes missing modules or preflights
    
    * Fix for recipes missing modules or preflights
    
    * Fix for command line options
    
    * Fix for command line options
    
    * Fix linter for command line options
    
    * Remove debugging call
    
    * Fix tests
    
    * Fix tests
    
    * Fix tests

--- tests/lib/state.py
+++ tests/lib/state.py
@@ -77,9 +77,10 @@ class StateTest(unittest.TestCase):
   def testProcessPreflightModules(self, mock_setup, mock_process):
     """Tests that preflight's process function is called correctly."""
     test_state = state.DFTimewolfState(config.Config)
+    test_state.command_line_options = {}
     test_state.LoadRecipe(test_recipe.contents)
     test_state.RunPreflights()
-    mock_setup.assert_called_with({})
+    mock_setup.assert_called_with()
     mock_process.assert_called_with()
 
   @mock.patch('tests.test_modules.modules.DummyModule2.SetUp')
@@ -87,8 +88,9 @@ class StateTest(unittest.TestCase):
   def testSetupModules(self, mock_setup1, mock_setup2):
     """Tests that module's setup functions are correctly called."""
     test_state = state.DFTimewolfState(config.Config)
+    test_state.command_line_options = {}
     test_state.LoadRecipe(test_recipe.contents)
-    test_state.SetupModules(DummyArgs())
+    test_state.SetupModules()
     mock_setup1.assert_called_with()
     mock_setup2.assert_called_with()
 
@@ -97,8 +99,9 @@ class StateTest(unittest.TestCase):
   def testProcessModules(self, mock_process1, mock_process2):
     """Tests that modules' process functions are correctly called."""
     test_state = state.DFTimewolfState(config.Config)
+    test_state.command_line_options = {}
     test_state.LoadRecipe(test_recipe.contents)
-    test_state.SetupModules(DummyArgs())
+    test_state.SetupModules()
     test_state.RunModules()
     mock_process1.assert_called_with()
     mock_process2.assert_called_with()
@@ -109,10 +112,11 @@ class StateTest(unittest.TestCase):
   def testProcessErrors(self, mock_exit, mock_process1, mock_process2):
     """Tests that module's errors are correctly caught."""
     test_state = state.DFTimewolfState(config.Config)
+    test_state.command_line_options = {}
     test_state.LoadRecipe(test_recipe.contents)
-    test_state.SetupModules(DummyArgs())
     mock_process1.side_effect = Exception('asd')
     mock_process2.side_effect = DFTimewolfError('dfTimewolf Error')
+    test_state.SetupModules()
     test_state.RunModules()
     mock_process1.assert_called_with()
     mock_process2.assert_called_with()
@@ -124,12 +128,6 @@ class StateTest(unittest.TestCase):
     msg, critical = sorted(test_state.global_errors, key=lambda x: x[0])[1]
     self.assertIn('dfTimewolf Error', msg)
     self.assertTrue(critical)
-
-
-# pylint: disable=no-init,unnecessary-pass
-class DummyArgs:
-  """Fake class to generate an object with an empty __dict__ attribute."""
-  pass
 
 if __name__ == '__main__':
   unittest.main()
