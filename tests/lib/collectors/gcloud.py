fbba62ce2c1e42f0fe32ec519e469b184ab246df
Thomas Chopitea <tomchop@gmail.com>
2020-03-27 17:05:53 +0100

    Fix tests

--- tests/lib/collectors/gcloud.py
+++ tests/lib/collectors/gcloud.py
@@ -57,16 +57,16 @@ class GoogleCloudCollectorTest(unittest.TestCase):
 
   # pylint: disable=invalid-name
   @mock.patch(
-      'libcloudforensics.gcp.GoogleComputeBaseResource.add_labels')
+      'libcloudforensics.gcp.GoogleComputeBaseResource.AddLabels')
   @mock.patch('libcloudforensics.gcp.GoogleComputeBaseResource')
-  @mock.patch('libcloudforensics.gcp.start_analysis_vm')
+  @mock.patch('libcloudforensics.gcp.StartAnalysisVm')
   def testSetUp(self,
-                mock_start_analysis_vm,
+                mock_StartAnalysisVm,
                 mock_GoogleComputeBaseResource,
-                mock_add_labels):
+                mock_AddLabels):
     """Tests that the collector can be initialized."""
     test_state = state.DFTimewolfState(config.Config)
-    mock_start_analysis_vm.return_value = (mock_GoogleComputeBaseResource, None)
+    mock_StartAnalysisVm.return_value = (mock_GoogleComputeBaseResource, None)
 
     gcloud_collector = gcloud.GoogleCloudCollector(test_state)
     gcloud_collector.SetUp(
@@ -88,7 +88,7 @@ class GoogleCloudCollectorTest(unittest.TestCase):
                      'my-owned-instance')
     self.assertEqual(gcloud_collector.all_disks, False)
 
-    mock_start_analysis_vm.assert_called_with(
+    mock_StartAnalysisVm.assert_called_with(
         'test-analysis-project-name',
         'gcp-forensics-vm-fake_incident_id',
         'fake_zone',
@@ -98,40 +98,40 @@ class GoogleCloudCollectorTest(unittest.TestCase):
         image_family='ubuntu-1804-lts',
         image_project='ubuntu-os-cloud'
     )
-    mock_add_labels.assert_has_calls(
+    mock_AddLabels.assert_has_calls(
         [mock.call({'incident_id': 'fake_incident_id'})])
 
   # pylint: disable=line-too-long
-  @mock.patch('libcloudforensics.gcp.GoogleComputeInstance.get_boot_disk')
-  @mock.patch('libcloudforensics.gcp.GoogleComputeBaseResource.add_labels')
-  @mock.patch('libcloudforensics.gcp.start_analysis_vm')
-  @mock.patch('libcloudforensics.gcp.GoogleCloudProject.create_disk_from_snapshot')
+  @mock.patch('libcloudforensics.gcp.GoogleComputeInstance.GetBootDisk')
+  @mock.patch('libcloudforensics.gcp.GoogleComputeBaseResource.AddLabels')
+  @mock.patch('libcloudforensics.gcp.StartAnalysisVm')
+  @mock.patch('libcloudforensics.gcp.GoogleCloudProject.CreateDiskFromSnapshot')
   @mock.patch('dftimewolf.lib.collectors.gcloud.GoogleCloudCollector._FindDisksToCopy')
-  @mock.patch('libcloudforensics.gcp.GoogleComputeDisk.snapshot')
-  @mock.patch('libcloudforensics.gcp.GoogleComputeSnapshot.delete')
-  @mock.patch('libcloudforensics.gcp.GoogleComputeInstance.attach_disk')
+  @mock.patch('libcloudforensics.gcp.GoogleComputeDisk.Snapshot')
+  @mock.patch('libcloudforensics.gcp.GoogleComputeSnapshot.Delete')
+  @mock.patch('libcloudforensics.gcp.GoogleComputeInstance.AttachDisk')
   def testProcess(self,
-                  unused_mock_attach_disk,
-                  mock_delete,
-                  mock_snapshot,
-                  mock_find_disks,
-                  mock_create_disk_from_snapshot,
-                  mock_start_analysis_vm,
-                  mock_add_labels,
-                  mock_get_boot_disk):
+                  unused_MockAttachDisk,
+                  mock_Delete,
+                  mock_Snapshot,
+                  mock_FindDisks,
+                  mock_CreateDiskFromSnapshot,
+                  mock_StartAnalysisVm,
+                  mock_AddLabels,
+                  mock_GetBootDisk):
     """Tests the collector's Process() function."""
-    mock_start_analysis_vm.return_value = (FAKE_ANALYSIS_VM, None)
-    mock_find_disks.return_value = [
+    mock_StartAnalysisVm.return_value = (FAKE_ANALYSIS_VM, None)
+    mock_FindDisks.return_value = [
         gcp.GoogleComputeDisk(
             FAKE_PROJECT,
             'fake_zone',
             'disk1')
     ]
-    mock_create_disk_from_snapshot.return_value = FAKE_DISK_COPY
-    mock_snapshot.return_value = FAKE_SNAPSHOT
-    FAKE_ANALYSIS_VM.add_labels = mock_add_labels
-    FAKE_ANALYSIS_VM.get_boot_disk = mock_get_boot_disk
-    FAKE_DISK_COPY.add_labels = mock_add_labels
+    mock_CreateDiskFromSnapshot.return_value = FAKE_DISK_COPY
+    mock_Snapshot.return_value = FAKE_SNAPSHOT
+    FAKE_ANALYSIS_VM.AddLabels = mock_AddLabels
+    FAKE_ANALYSIS_VM.GetBootDisk = mock_GetBootDisk
+    FAKE_DISK_COPY.AddLabels = mock_AddLabels
 
     test_state = state.DFTimewolfState(config.Config)
     gcloud_collector = gcloud.GoogleCloudCollector(test_state)
@@ -146,38 +146,38 @@ class GoogleCloudCollectorTest(unittest.TestCase):
     )
     gcloud_collector.Process()
 
-    mock_snapshot.assert_called_once()
-    mock_create_disk_from_snapshot.assert_called_with(
+    mock_Snapshot.assert_called_once()
+    mock_CreateDiskFromSnapshot.assert_called_with(
         FAKE_SNAPSHOT, disk_name_prefix='incidentfake_incident_id')
-    mock_delete.assert_called_once()
+    mock_Delete.assert_called_once()
     self.assertEqual(test_state.output[0][0], 'fake-analysis-vm')
     self.assertEqual(test_state.output[0][1].name, 'disk1-copy')
-    mock_add_labels.assert_has_calls([mock.call({'incident_id': 'fake_incident_id'})])
+    mock_AddLabels.assert_has_calls([mock.call({'incident_id': 'fake_incident_id'})])
 
   # pylint: disable=line-too-long,invalid-name
   @mock.patch('libcloudforensics.gcp.GoogleComputeBaseResource')
-  @mock.patch('libcloudforensics.gcp.GoogleComputeInstance.get_boot_disk')
-  @mock.patch('libcloudforensics.gcp.GoogleCloudProject.get_disk')
-  @mock.patch('libcloudforensics.gcp.GoogleComputeInstance.list_disks')
-  @mock.patch('libcloudforensics.gcp.GoogleCloudProject.get_instance')
-  @mock.patch('libcloudforensics.gcp.start_analysis_vm')
+  @mock.patch('libcloudforensics.gcp.GoogleComputeInstance.GetBootDisk')
+  @mock.patch('libcloudforensics.gcp.GoogleCloudProject.GetDisk')
+  @mock.patch('libcloudforensics.gcp.GoogleComputeInstance.ListDisks')
+  @mock.patch('libcloudforensics.gcp.GoogleCloudProject.GetInstance')
+  @mock.patch('libcloudforensics.gcp.StartAnalysisVm')
   # We're manually calling protected functions
   # pylint: disable=protected-access
   def testFindDisksToCopy(self,
-                          mock_start_analysis_vm,
+                          mock_StartAnalysisVm,
                           mock_get_instance,
                           mock_list_disks,
                           mock_get_disk,
-                          mock_get_boot_disk,
+                          mock_GetBootDisk,
                           mock_GoogleComputeBaseResource):
     """Tests the FindDisksToCopy function with different SetUp() calls."""
     test_state = state.DFTimewolfState(config.Config)
     gcloud_collector = gcloud.GoogleCloudCollector(test_state)
-    mock_start_analysis_vm.return_value = (mock_GoogleComputeBaseResource, None)
+    mock_StartAnalysisVm.return_value = (mock_GoogleComputeBaseResource, None)
     mock_list_disks.return_value = ['bootdisk', 'disk1']
     mock_get_disk.side_effect = ReturnFakeDisk
     mock_get_instance.return_value = FAKE_INSTANCE
-    mock_get_boot_disk.return_value = FAKE_BOOT_DISK
+    mock_GetBootDisk.return_value = FAKE_BOOT_DISK
 
     # Nothing is specified, GoogleCloudCollector should collect the instance's
     # boot disk
@@ -193,7 +193,7 @@ class GoogleCloudCollectorTest(unittest.TestCase):
     disks = gcloud_collector._FindDisksToCopy()
     self.assertEqual(len(disks), 1)
     self.assertEqual(disks[0].name, 'bootdisk')
-    mock_get_boot_disk.assert_called_once()
+    mock_GetBootDisk.assert_called_once()
 
     # Specifying all_disks should return all disks for the instance
     # (see mock_list_disks return value)
