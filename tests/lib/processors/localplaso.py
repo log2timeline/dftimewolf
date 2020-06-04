3c35d4471e276360e428a2dc96a7fb478650ced1
Thomas Chopitea <tomchop@gmail.com>
2020-06-04 15:06:58 +0200

    Fix linter

--- tests/lib/processors/localplaso.py
+++ tests/lib/processors/localplaso.py
@@ -25,6 +25,7 @@ class LocalPlasoTest(unittest.TestCase):
 
   @mock.patch('subprocess.Popen')
   def testProcessing(self, mock_Popen):
+    """Tests that the correct number of containers is added."""
     test_state = state.DFTimewolfState(config.Config)
     mock_popen_object = mock.Mock()
     mock_popen_object.communicate.return_value = (None, None)
@@ -32,7 +33,8 @@ class LocalPlasoTest(unittest.TestCase):
     mock_Popen.return_value = mock_popen_object
 
     local_plaso_processor = localplaso.LocalPlasoProcessor(test_state)
-    test_state.StoreContainer(containers.File(name='test', path='/notexist/test'))
+    test_state.StoreContainer(
+        containers.File(name='test', path='/notexist/test'))
     local_plaso_processor.SetUp()
     local_plaso_processor.Process()
     mock_Popen.assert_called_once()
@@ -40,8 +42,8 @@ class LocalPlasoTest(unittest.TestCase):
     self.assertEqual(args[9], '/notexist/test')
     plaso_path = args[8] # Dynamically generated path to the plaso file
     self.assertEqual(
-      test_state.GetContainers(containers.File)[0].path,
-      plaso_path)
+        test_state.GetContainers(containers.File)[0].path,
+        plaso_path)
 
 if __name__ == '__main__':
   unittest.main()
