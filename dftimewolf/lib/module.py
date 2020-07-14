9fbde57fed402d12d2205f5954db19467bc5b3c7
Thomas Chopitea <tomchop@gmail.com>
2020-07-14 13:51:04 +0200

    Better errors (core)

--- dftimewolf/lib/module.py
+++ dftimewolf/lib/module.py
@@ -3,7 +3,10 @@
 
 import abc
 import logging
+import traceback
+import sys
 
+from dftimewolf.lib import errors
 from dftimewolf.lib import logging_utils
 
 class BaseModule(object):
@@ -33,10 +36,42 @@ class BaseModule(object):
     console_handler.setFormatter(formatter)
     self.logger.addHandler(console_handler)
 
+  @property
+  def name(self):
+    """Returns the class name for this module."""
+    return self.__class__.__name__
+
   def CleanUp(self):
     """Cleans up module output to prepare it for the next module."""
     # No clean up is required.
     return
+
+  def ModuleError(self, message, critical=False):
+    """Declares a module error.
+
+    Errors will be stored in a DFTimewolfError error object and passed on to the
+    state. Critical errors will also raise a DFTimewolfError error.
+
+    Args:
+      message (str): Error text.
+      critical (Optional[bool]): True if dfTimewolf cannot recover from
+          the error and should abort execution.
+
+    Raises:
+      errors.DFTimewolfError: If the error is critical and dfTimewolf
+          should abort execution of the recipe.
+    """
+    stacktrace = None
+    if sys.exc_info() != (None, None, None):
+      stacktrace = traceback.format_exc()
+
+    error = errors.DFTimewolfError(
+        message, name=self.name, stacktrace=stacktrace, critical=critical)
+    self.state.AddError(error)
+    if critical:
+      self.logger.critical(error.message)
+      raise error
+    self.logger.error(error.message)
 
   @abc.abstractmethod
   def Process(self):
