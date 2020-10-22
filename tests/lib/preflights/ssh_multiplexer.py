75d8dfbafb555403eb577737bebfe4bd2388a18a
Thomas Chopitea <tomchop@gmail.com>
2020-10-22 14:38:09 +0200

    Tests

---
+++ tests/lib/preflights/ssh_multiplexer.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Tests for the SSH multiplexer preflight."""
+
+import unittest
+import mock
+
+from dftimewolf.lib import state
+from dftimewolf.lib import errors
+from dftimewolf.lib.preflights import ssh_multiplexer
+
+from dftimewolf import config
+
+
+class SSHMultiplexer(unittest.TestCase):
+  """Tests for the SSH multiplexer preflight."""
+
+  def testInitialization(self):
+    """Tests that the exporter can be initialized."""
+    test_state = state.DFTimewolfState(config.Config)
+    local_ssh_multiplexer = ssh_multiplexer.SSHMultiplexer(test_state)
+    self.assertIsNotNone(local_ssh_multiplexer)
+
+  @mock.patch('subprocess.call')
+  def testProcess(self, mock_call):
+    mock_call.return_value = 0
+    test_state = state.DFTimewolfState(config.Config)
+    ssh_multi = ssh_multiplexer.SSHMultiplexer(test_state)
+    ssh_multi.SetUp('fakeuser', 'fakehost', None)
+    ssh_multi.Process()
+
+    mock_call.assert_called_with([
+      'ssh', '-q', '-l', 'fakeuser', 'fakehost', 'true',
+      '-o', 'ControlMaster=auto', '-o', 'ControlPersist=yes',
+      '-o', 'ControlPath= ~/.ssh/ctrl-%C'
+    ])
+
+  @mock.patch('subprocess.call')
+  def testCleanup(self, mock_call):
+    mock_call.return_value = 0
+    test_state = state.DFTimewolfState(config.Config)
+    ssh_multi = ssh_multiplexer.SSHMultiplexer(test_state)
+    ssh_multi.SetUp('fakeuser', 'fakehost', None)
+    ssh_multi.CleanUp()
+
+    mock_call.assert_called_with([
+      'ssh', '-O', 'exit', 'fakehost'
+    ])
+
+
+
+if __name__ == '__main__':
+  unittest.main()
