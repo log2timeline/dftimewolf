92d82cc43efb6fcfc6ba48fc26ff0e071f6e9a1d
Romain Gayon <romaing@google.com>
2020-11-09 18:37:20 +0100

    init

---
+++ dftimewolf/lib/preflights/cloud_token.py
@@ -0,0 +1,34 @@
+"""Checks for proper authentication to Cloud platforms."""
+
+import shutil
+import subprocess
+
+from dftimewolf.lib import module
+from dftimewolf.lib.modules import manager as modules_manager
+
+
+class GCPTokenCheck(module.PreflightModule):
+  """Checks for GCP authentication."""
+
+  def SetUp(self, project):
+    """Runs gcloud to make sure we can authenticate to GCP APIs.
+
+    Args:
+      project(string): the project we want to connect to.
+    """
+
+    gcloud_path = shutil.which('gcloud')
+    if not gcloud_path:
+      self.ModuleError(
+          'Could not find path to gcloud tool. Please install gcloud SDK, as it '
+          'required to authenticate to GCP')
+    try:
+      subprocess.check_output(
+          ['gcloud', 'auth', 'application-default', 'print-access-token', '--project', project])  # pylint: disable=line-too-long
+    except subprocess.CalledProcessError:
+      self.ModuleError(
+          'Your GCP application credentials are invalid. Please run '
+          'gcloud auth application-default login')
+
+
+modules_manager.ModulesManager.RegisterModule(GCPTokenCheck)
