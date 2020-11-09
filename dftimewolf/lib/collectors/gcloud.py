d23a0dd5108096706109e48f4160e3954beb93f3
Ramo Johnstone <ramoj@google.com>
2020-11-09 16:33:49 +1100

    tmp revert for build errors

--- dftimewolf/lib/collectors/gcloud.py
+++ dftimewolf/lib/collectors/gcloud.py
@@ -3,7 +3,7 @@
 
 from google.auth.exceptions import DefaultCredentialsError, RefreshError
 from googleapiclient.errors import HttpError
-from libcloudforensics.errors import ResourceNotFoundError
+#from libcloudforensics.errors import ResourceNotFoundError
 from libcloudforensics.providers.gcp.internal import project as gcp_project
 from libcloudforensics.providers.gcp import forensics as gcp_forensics
 
@@ -161,15 +161,15 @@ class GoogleCloudCollector(module.BaseModule):
             type_=self._ANALYSIS_VM_CONTAINER_ATTRIBUTE_TYPE,
             value=analysis_vm_name))
 
-    try:
-      if self.remote_instance_name:
-        self.remote_project.compute.GetInstance(self.remote_instance_name)
-    except ResourceNotFoundError as exception:
-      self.ModuleError(
-        message='Instance "{0:s}" not found or insufficient permissions'.format(
-          self.remote_instance_name),
-        critical=True)
-      return
+#    try:
+#      if self.remote_instance_name:
+#        self.remote_project.compute.GetInstance(self.remote_instance_name)
+#    except ResourceNotFoundError as exception:
+#      self.ModuleError(
+#        message='Instance "{0:s}" not found or insufficient permissions'.format(
+#          self.remote_instance_name),
+#        critical=True)
+#      return
 
     try:
       # TODO: Make creating an analysis VM optional
