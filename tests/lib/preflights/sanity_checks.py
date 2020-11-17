e5aa4417c77b95d0fb0d9cf4fd00a82d6a827ca2
Ramo Johnstone <ramoj@google.com>
2020-11-17 00:44:21 +0000

    Adjustments for locally working tests

--- tests/lib/preflights/sanity_checks.py
+++ tests/lib/preflights/sanity_checks.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Tests for the SSH multiplexer preflight."""
+"""Tests for the Sanity Checker preflight."""
 
 import unittest
-import mock
 
 from dftimewolf.lib import state
-from dftimewolf.lib.preflights.sanity_checks import SanityChecks
+from dftimewolf.lib.preflights import sanity_checks
 from dftimewolf import config
 
 
@@ -15,45 +14,46 @@ class SanityChecks(unittest.TestCase):
 
   def testInitialization(self):
     test_state = state.DFTimewolfState(config.Config)
-    checker = SanityChecks(test_state)
+    checker = sanity_checks.SanityChecks(test_state)
     self.assertIsNotNone(checker)
 
   def testInvalidDateOrder(self):
     test_state = state.DFTimewolfState(config.Config)
-    checker = SanityChecks(test_state)
+    checker = sanity_checks.SanityChecks(test_state)
 
     checker.SetUp(
         startdate='2020-10-31', enddate='2020-10-01', dateformat='%Y-%m-%d')
     checker.Process()
 
     # The incorrect date order should mean an error was encountered
-    self.assertLen(
-        checker.state.errors, 1,
+    self.assertEqual(
+        len(checker.state.errors), 1,
         'Date order validation succeeded when it should have failed')
 
   def testInvalidDateFormat(self):
     test_state = state.DFTimewolfState(config.Config)
-    checker = SanityChecks(test_state)
+    checker = sanity_checks.SanityChecks(test_state)
 
     checker.SetUp(
         startdate='20-10-01', enddate='20-10-31', dateformat='%Y-%m-%d')
     checker.Process()
 
     # The incorrect date format should mean an error was encountered
-    self.assertLen(
-        checker.state.errors, 1,
+    self.assertEqual(
+        len(checker.state.errors), 1,
         'Date format validation succeeded when it should have failed')
 
   def testValidDates(self):
     test_state = state.DFTimewolfState(config.Config)
-    checker = SanityChecks(test_state)
+    checker = sanity_checks.SanityChecks(test_state)
 
     checker.SetUp(
         startdate='2020-10-01', enddate='2020-10-31', dateformat='%Y-%m-%d')
     checker.Process()
 
     # All good - should be no errors
-    self.assertEmpty(checker.state.errors, 'Date order validation failure')
+    self.assertEqual(
+        len(checker.state.errors), 0, 'Date order validation failure')
 
 if __name__ == '__main__':
   unittest.main()
