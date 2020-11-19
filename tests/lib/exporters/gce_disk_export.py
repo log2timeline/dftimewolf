fc2b6fcf4515b9a304ca98ad6d8783a8bef1a283
Said Eid <46205691+sa3eed3ed@users.noreply.github.com>
2020-11-19 18:34:19 +0200

    PR1/2 Export-> GCS -> Import -> Turbinia -> TS (#317)
    * - Disk export recipe to accept a list of disks or
    an evidence VM as input and export all of them to GCS
    - Changing unittests
    - Create analysis VM is made optional.
    - cpu_cores as an optional argument with default of 4
    - Recipes description nits
    - Incident ID is optional: not affecting the current default behavior
    - if no incident ID default is appending timestamp to the
    analysis VM prefix
    - Zone is optional for analysis VM
    
    * PR 317 comments
    
    * comments PR 317
    
    * pr 317 comments
    
    * ut fix
    
    * fix linter

--- tests/lib/exporters/gce_disk_export.py
+++ tests/lib/exporters/gce_disk_export.py
@@ -40,34 +40,38 @@ class GoogleCloudDiskExportTest(unittest.TestCase):
   # pylint: disable=line-too-long
   @mock.patch('libcloudforensics.providers.gcp.internal.compute.GoogleCloudCompute.GetDisk')
   @mock.patch('libcloudforensics.providers.gcp.internal.project.GoogleCloudProject')
-  def testSetUp(self, mock_gcp_project, mock_get_disk):
+  def testSetUp(
+      self,
+      mock_gcp_project,
+      mock_get_disk):
     """Tests that the exporter can be initialized."""
 
     test_state = state.DFTimewolfState(config.Config)
     cloud_disk_exporter = gce_disk_export.GoogleCloudDiskExport(
         test_state)
     mock_gcp_project.return_value = FAKE_SOURCE_PROJECT
-    FAKE_SOURCE_PROJECT.GetDisk = mock_get_disk
+    FAKE_SOURCE_PROJECT.compute.GetDisk = mock_get_disk
     mock_get_disk.return_value = FAKE_DISK
     cloud_disk_exporter.SetUp(
         'fake-source-project',
-        'fake-source-disk',
         'gs://fake-bucket',
         None,
-        None
+        'fake-source-disk',
+        None,
+        False,
+        'image-df-export-temp'
     )
     self.assertEqual(test_state.errors, [])
     self.assertEqual(cloud_disk_exporter.analysis_project.project_id,
                      'fake-source-project')
     self.assertEqual(cloud_disk_exporter.source_project.project_id,
                      'fake-source-project')
-    self.assertEqual(cloud_disk_exporter.source_disk.name,
+    self.assertEqual(cloud_disk_exporter.source_disks[0].name,
                      'fake-source-disk')
     self.assertEqual(cloud_disk_exporter.gcs_output_location,
                      'gs://fake-bucket')
-    self.assertEqual(cloud_disk_exporter.exported_disk_name,
-                     '{0:s}-image-df-export-temp'.format(
-                         'fake-source-disk'))
+    self.assertEqual(cloud_disk_exporter.exported_image_name,
+                     'image-df-export-temp')
 
   # pylint: disable=line-too-long
   @mock.patch('libcloudforensics.providers.gcp.internal.compute.GoogleComputeImage.Delete')
@@ -87,30 +91,26 @@ class GoogleCloudDiskExportTest(unittest.TestCase):
     cloud_disk_exporter = gce_disk_export.GoogleCloudDiskExport(
         test_state)
     mock_gcp_project.return_value = FAKE_SOURCE_PROJECT
-    FAKE_SOURCE_PROJECT.GetDisk = mock_get_disk
+    FAKE_SOURCE_PROJECT.compute.GetDisk = mock_get_disk
     mock_get_disk.return_value = FAKE_DISK
     cloud_disk_exporter.SetUp(
-        'fake-source-project',
-        'fake-source-disk',
-        'gs://fake-bucket',
-        None,
-        None
+        source_project_name='fake-source-project',
+        gcs_output_location='gs://fake-bucket',
+        source_disk_names='fake-source-disk',
+        exported_image_name='image-df-export-temp'
     )
-    FAKE_SOURCE_PROJECT.CreateImageFromDisk = mock_create_image_from_disk
+    FAKE_SOURCE_PROJECT.compute.CreateImageFromDisk = mock_create_image_from_disk
     mock_create_image_from_disk.return_value = FAKE_IMAGE
     FAKE_IMAGE.ExportImage = mock_export_image
     FAKE_IMAGE.Delete = mock_delete_image
     cloud_disk_exporter.Process()
     mock_create_image_from_disk.assert_called_with(
-        FAKE_DISK, name='{0:s}-image-df-export-temp'.format(
-            'fake-source-disk'))
+        FAKE_DISK)
     mock_export_image.assert_called_with(
-        'gs://fake-bucket', output_name='{0:s}-image-df-export-temp'.format(
-            'fake-source-disk'))
+        'gs://fake-bucket', output_name='image-df-export-temp')
     mock_delete_image.assert_called_once()
     output_url = os.path.join(
-        'gs://fake-bucket', '{0:s}-image-df-export-temp.tar.gz'.format(
-            'fake-source-disk'))
+        'gs://fake-bucket', 'image-df-export-temp.tar.gz')
     urls = test_state.GetContainers(containers.URL)
     self.assertEqual(urls[0].path, output_url)
 
