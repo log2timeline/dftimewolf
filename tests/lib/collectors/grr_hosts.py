5b3cf658e0824daded20e9d254073851ca12a6f8
Thomas Chopitea <tomchop@gmail.com>
2021-04-22 12:12:26 +0200

    Add capability to select GRR clients by username (#372)
    * Add filtering function
    
    * Fix bugs
    
    * Shorten docstring
    
    * Minor test fix

--- tests/lib/collectors/grr_hosts.py
+++ tests/lib/collectors/grr_hosts.py
@@ -51,8 +51,15 @@ class GRRFlowTests(unittest.TestCase):
         mock_grr_hosts.MOCK_CLIENT_LIST
     client = self.grr_flow_module._GetClientBySelector('C.0000000000000001')
     self.mock_grr_api.SearchClients.assert_called_with('C.0000000000000001')
-    self.assertEqual(
-        client.data.client_id, mock_grr_hosts.MOCK_CLIENT_RECENT.data.client_id)
+    self.assertEqual(client.data.client_id, 'C.0000000000000001')
+
+  def testGetClientByUsername(self):
+    """Tests that GetClientBySelector fetches the correct GRR client."""
+    self.mock_grr_api.SearchClients.return_value = \
+        mock_grr_hosts.MOCK_CLIENT_LIST
+    client = self.grr_flow_module._GetClientBySelector('tomchop_username2')
+    self.mock_grr_api.SearchClients.assert_called_with('tomchop_username2')
+    self.assertEqual(client.data.client_id, 'C.0000000000000001')
 
   def testGetClientBySelectorError(self):
     """Tests that GetClientBySelector fetches the most recent GRR client."""
