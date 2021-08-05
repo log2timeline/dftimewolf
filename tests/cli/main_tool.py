855d70f6c84686aeccf62f40596e02936aae4cec
Thomas Chopitea <tomchop@gmail.com>
2021-08-05 15:59:59 +0200

    Add test to check that modules are invoked with the expected parameters. (#413)
    * Add test to check that modules are invoked with the expected parameters.
    
    * Fix formatting
    
    * Add missing attribute from docstring
    
    * Try having pipfile follow turbinia/master
    
    * Fix tests
    
    * Update pipfile and AWS mocks
    
    * Update requirements

--- tests/cli/main_tool.py
+++ tests/cli/main_tool.py
@@ -4,15 +4,24 @@
 
 import unittest
 import logging
+import inspect
 
 from dftimewolf.cli import dftimewolf_recipes
-
+from dftimewolf.lib import state as dftw_state
+from dftimewolf import config
 
 class MainToolTest(unittest.TestCase):
   """Tests for main tool functions."""
 
   def setUp(self):
-    pass
+    self.tool = dftimewolf_recipes.DFTimewolfTool()
+    self.tool.LoadConfiguration()
+    self.tool.ReadRecipes()
+
+  def tearDown(self):
+    # pylint: disable=protected-access
+    for recipe in self.tool._recipes_manager.GetRecipes():
+      self.tool._recipes_manager.DeregisterRecipe(recipe)
 
   def testSetupLogging(self):
     """Tests the SetupLogging function."""
@@ -24,15 +33,31 @@ class MainToolTest(unittest.TestCase):
 
   def testToolWithArbitraryRecipe(self):
     """Tests that recipes are read and valid, and an exec plan is logged."""
-    tool = dftimewolf_recipes.DFTimewolfTool()
-    tool.LoadConfiguration()
-    tool.ReadRecipes()
     # We want to ensure that recipes are loaded (10 is arbitrary)
     # pylint: disable=protected-access
-    self.assertGreater(len(tool._recipes_manager._recipes), 10)
+    self.assertGreater(len(self.tool._recipes_manager._recipes), 10)
     # Conversion to parse arguments is done within ParseArguments
     # We can pass an arbitrary recipe with valid args here.
-    tool.ParseArguments(['upload_ts', '/tmp/test'])
-    tool.state.LogExecutionPlan()
-    for recipe in tool._recipes_manager.GetRecipes():
-      tool._recipes_manager.DeregisterRecipe(recipe)
+    self.tool.ParseArguments(['upload_ts', '/tmp/test'])
+    self.tool.state.LogExecutionPlan()
+
+  def testRecipeSetupArgs(self):
+    """Checks that all recipes pass the correct arguments to their modules."""
+    # We want to access the tool's sate object to load recipes and go through
+    # modules.
+    # pylint: disable=protected-access
+    self.tool._state =  dftw_state.DFTimewolfState(config.Config)
+
+    for recipe in self.tool._recipes_manager.GetRecipes():
+      self.tool._state.LoadRecipe(recipe.contents, dftimewolf_recipes.MODULES)
+      for module in recipe.contents['modules']:
+        runtime_name = module.get('runtime_name', module['name'])
+        setup_func = self.tool.state._module_pool[runtime_name].SetUp
+        expected_args = set(inspect.getfullargspec(setup_func).args)
+        expected_args.remove('self')
+        provided_args = set(module['args'])
+
+        self.assertEqual(
+          expected_args,
+          provided_args,
+          f'Error in {recipe.name}:{runtime_name}')
