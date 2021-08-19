2a26599f5c3b838a430035ae42b2d3f76b2d28da
Daniel White <onager@deerpie.com>
2021-08-19 12:12:01 +0200

    Misc fixes for workspace audit logs

--- dftimewolf/lib/processors/workspace_audit_timesketch.py
+++ dftimewolf/lib/processors/workspace_audit_timesketch.py
@@ -95,7 +95,7 @@ class WorkspaceAuditTimesketch(BaseModule):
     """
     application_name = timesketch_record.get('applicationName')
     format_strings = self._all_application_format_strings.get(application_name)
-    event_name = timesketch_record.get('_event_name')
+    event_name = timesketch_record.get('_event_name', '').lower()
     format_string = format_strings.get(event_name)
 
     message = ''
@@ -178,8 +178,9 @@ class WorkspaceAuditTimesketch(BaseModule):
           output_file.write('\n')
     output_file.close()
 
-    timeline_name = 'Workspace {0:s} logs {1:s}"'.format(
-        logs_container.application_name, logs_container.filter_expression)
+    timeline_name = 'Workspace {0:s} logs {1:s} {2:s}"'.format(
+        logs_container.application_name, logs_container.user_key,
+        logs_container.filter_expression)
 
     container = containers.File(name=timeline_name, path=output_path)
     self.state.StoreContainer(container)
