a88647ee5c7e28010b2a88041866c2034982b34f
ramo-j <ramoj@google.com>
2022-01-14 06:37:27 +0000

    In progress

--- tests/lib/processors/turbinia_gcp.py
+++ tests/lib/processors/turbinia_gcp.py
@@ -7,6 +7,7 @@ import unittest
 import mock
 import six
 
+from libcloudforensics.providers.gcp.internal.compute import GoogleComputeDisk
 from dftimewolf.lib import state
 from dftimewolf.lib import errors
 
@@ -139,12 +140,13 @@ class TurbiniaProcessorTest(unittest.TestCase):
   @mock.patch('turbinia.evidence.GoogleCloudDisk')
   @mock.patch('turbinia.client.get_turbinia_client')
   # pylint: disable=invalid-name
-  def testProcess(self,
+  def testProcessFromParams(self,
                   _mock_TurbiniaClient,
                   mock_GoogleCloudDisk,
                   mock_GCSOutputWriter,
                   mock_exists):
-    """Tests that the processor processes data correctly."""
+    """Tests that the processor processes data correctly when a disk name is
+    passed in as a parameter."""
 
     test_state = state.DFTimewolfState(config.Config)
     test_state.StoreContainer(
@@ -155,6 +157,96 @@ class TurbiniaProcessorTest(unittest.TestCase):
     turbinia_processor.SetUp(
         turbinia_config_file=None,
         disk_names='disk-1',
+        project='turbinia-project',
+        turbinia_zone='europe-west1',
+        sketch_id=4567,
+        run_all_jobs=False)
+
+    turbinia_processor.client.get_task_data.return_value = [{
+        'saved_paths': [
+            '/fake/data.plaso',
+            '/fake/data2.plaso',
+            '/another/random/file.txt',
+            'gs://BinaryExtractorTask.tar.gz',
+        ]
+    }]
+
+    # Return true so the tests assumes the above file exists
+    mock_exists.return_value = True
+
+    # Our GS path will be downloaded to this fake local path
+    local_mock = mock.MagicMock()
+    local_mock.copy_from.return_value = '/local/BinaryExtractorTask.tar.gz'
+    mock_GCSOutputWriter.return_value = local_mock
+
+    turbinia_processor.PreProcess()
+    in_containers = test_state.GetContainers(
+        turbinia_processor.GetThreadOnContainerType())
+    for c in in_containers:
+      turbinia_processor.Process(c)
+    turbinia_processor.PostProcess()
+
+    mock_GoogleCloudDisk.assert_called_with(
+        disk_name='disk-1',
+        project='turbinia-project',
+        zone='europe-west1')
+
+    # These are mock classes, so there is a member
+    # pylint: disable=no-member
+    turbinia_processor.client.send_request.assert_called()
+    request = turbinia_processor.client.send_request.call_args[0][0]
+    self.assertEqual(request.recipe['globals']['sketch_id'], 4567)
+    self.assertListEqual(
+        request.recipe['globals']['jobs_denylist'],
+        ['StringsJob', 'BinaryExtractorJob', 'BulkExtractorJob', 'PhotorecJob'])
+    turbinia_processor.client.get_task_data.assert_called()
+    self.assertEqual(
+      request.recipe['globals']['yara_rules'],
+      "rule dummy { condition: false }"
+    )
+
+    # pylint: disable=protected-access
+    mock_GCSOutputWriter.assert_any_call(
+        'gs://BinaryExtractorTask.tar.gz',
+        local_output_dir=turbinia_processor._output_path
+    )
+    self.assertEqual(test_state.errors, [])
+    ti_containers = test_state.GetContainers(containers.ThreatIntelligence)
+    file_containers = test_state.GetContainers(containers.File)
+
+    # Make sure that file.txt is ignored
+    self.assertEqual(len(file_containers), 2)
+
+    self.assertEqual(ti_containers[0].name, 'BinaryExtractorResults')
+    self.assertEqual(ti_containers[0].path, '/local/BinaryExtractorTask.tar.gz')
+
+    self.assertEqual(file_containers[0].name, 'turbinia-project-disk-1')
+    self.assertEqual(file_containers[1].name, 'turbinia-project-disk-1')
+    self.assertEqual(file_containers[0].path, '/fake/data.plaso')
+    self.assertEqual(file_containers[1].path, '/fake/data2.plaso')
+
+  @mock.patch('os.path.exists')
+  @mock.patch('turbinia.output_manager.GCSOutputWriter')
+  @mock.patch('turbinia.evidence.GoogleCloudDisk')
+  @mock.patch('turbinia.client.get_turbinia_client')
+  # pylint: disable=invalid-name
+  def testProcessFromState(self,
+                  _mock_TurbiniaClient,
+                  mock_GoogleCloudDisk,
+                  mock_GCSOutputWriter,
+                  mock_exists):
+    """Tests that the processor processes data correctly when a GCEDisk is
+    received from the state.
+    """
+    test_state = state.DFTimewolfState(config.Config)
+    test_state.StoreContainer(
+      containers.YaraRule(
+        name='dummy_yara', rule_text="rule dummy { condition: false }")
+    )
+    test_state.StoreContainer(containers.GCEDisk(name='disk-1'))
+    turbinia_processor = turbinia_gcp.TurbiniaGCPProcessor(test_state)
+    turbinia_processor.SetUp(
+        turbinia_config_file=None,
         project='turbinia-project',
         turbinia_zone='europe-west1',
         sketch_id=4567,
@@ -256,9 +348,38 @@ class TurbiniaProcessorTest(unittest.TestCase):
         local_paths, ['/local/path.plaso', '/tmp/BinaryExtractorTask.tar.gz'])
     self.assertEqual(gs_paths, ['gs://hashes.json'])
 
-  def testPreProcess(self):
-    """Tests that ForensicsVM containers are picked up properly."""
-    self.assertTrue(False) # Implement
+  @mock.patch('turbinia.client.get_turbinia_client')
+  def testPreProcess(self, mockTurbiniaClient):
+    """Tests that ForensicsVM containers are picked up properly.
+    
+    We store a ForensicsVM container in the state, run PreProcess, then check
+    that the state containes a GCEDisk as expected."""
+    test_state = state.DFTimewolfState(config.Config)
+    test_state.StoreContainer(containers.ForensicsVM(      
+         name='ForensicsVM',
+         evidence_disk=GoogleComputeDisk(
+           name='disk-1',
+           project_id='turbinia-project',
+           zone='europe-west1'),
+         platform = 'gcp'))
+
+
+    print(test_state.store)
+    
+    turbinia_processor = turbinia_gcp.TurbiniaGCPProcessor(test_state)
+    turbinia_processor.SetUp(
+        turbinia_config_file=None,
+        project='turbinia-project',
+        turbinia_zone='europe-west1',
+        sketch_id=4567,
+        run_all_jobs=False)
+    turbinia_processor.PreProcess()
+
+    out_containers = test_state.GetContainers(containers.GCEDisk)
+
+    self.assertEqual(len(out_containers), 1)
+    self.assertEqual(out_containers[0].name, 'disk-1')
+
 
 if __name__ == '__main__':
   unittest.main()
