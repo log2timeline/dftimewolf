10bc71f4172fe09df47600df67dd8167900f39f1
Jonathan Greig <jonathangreig@google.com>
2022-02-04 11:16:16 +1100

    Add unit tests

---
+++ tests/lib/collectors/azure_logging.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Tests the AWS logging collector."""
+
+import unittest
+from datetime import datetime as dt
+from unittest.mock import MagicMock
+
+import mock
+
+from dftimewolf.lib import state
+from dftimewolf import config
+from dftimewolf.lib.collectors import azure_logging
+from dftimewolf.lib.containers.containers import AzureLogs
+
+
+class AzureLogging(unittest.TestCase):
+  """Tests for the Azure logging collector."""
+
+  def testInitialization(self):
+    """Tests that the collector can be initialized."""
+    test_state = state.DFTimewolfState(config.Config)
+    azure_logging_collector = azure_logging.AzureLogsCollector(test_state)
+    self.assertIsNotNone(azure_logging_collector)
+
+  @mock.patch('azure.identity.DefaultAzureCredential')
+  @mock.patch('azure.mgmt.monitor.MonitorManagementClient')
+  def testProcess(self, mock_monitor, mock_identity):
+    """Tests that the Azure monitor client is called with the correct args."""
+
+    # Create mock objects with required attributes - not mocking Azure objects
+    # directly as this leads to frail mocks based on version-dependent package
+    # names like azure.mgmt.monitor.v2015_04_01.models._models_py3.EventData.
+    mock_monitor_client = MagicMock(spec=['activity_logs'])
+    mock_activity_logs_client = MagicMock(spec=['list'])
+    mock_event_data = MagicMock(spec=['as_dict'])
+
+    mock_monitor_client.activity_logs = mock_activity_logs_client
+    mock_activity_logs_client.list.return_value = iter([mock_event_data])
+    mock_event_data.as_dict.return_value = {'log_entry': 1}
+
+    mock_monitor.return_value = mock_monitor_client
+    mock_identity.return_value = 'Credentials'
+
+    test_state = state.DFTimewolfState(config.Config)
+    azure_logging_collector = azure_logging.AzureLogsCollector(test_state)
+
+    azure_logging_collector.SetUp(
+        subscription_id='55c5ff71-b3e2-450d-89da-cb12c1a38d87',
+        filter_expression='eventTimestamp ge \'2022-02-01\'')
+    azure_logging_collector.Process()
+
+    mock_monitor.assert_called_with(
+        'Credentials', '55c5ff71-b3e2-450d-89da-cb12c1a38d87')
+    mock_activity_logs_client.list.assert_called_with(
+        filter='eventTimestamp ge \'2022-02-01\'')
+    self.assertTrue(test_state.GetContainers(AzureLogs))
+    
