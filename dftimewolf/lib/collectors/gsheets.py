01097543a0133fbd94cb07ac6964c831368eeee8
Hussein Khalifa <hkhalifa@google.com>
2022-02-07 21:16:31 +0000

    Fixed review comments.

--- dftimewolf/lib/collectors/gsheets.py
+++ dftimewolf/lib/collectors/gsheets.py
@@ -16,7 +16,6 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient import discovery
-from pandas.core.frame import DataFrame
 
 from dftimewolf.lib import module
 from dftimewolf.lib.containers import containers
@@ -39,25 +38,26 @@ class GoogleSheetsCollector(module.BaseModule):
     """Initializes a Google Sheets collector."""
     super(GoogleSheetsCollector, self).__init__(
         state, name=name, critical=critical)
+    self._sheets_resource = None
     self._credentials = None
     self._spreadsheet_id = ''
     self._sheet_names: List[str] = []
     # These are mandatory columns required by Timesketch.
     self._mandatory_columns = ['message', 'datetime', 'timestamp_desc']
-    self._all_sheets = True
+    self._all_sheets = False
     self._validate_columns = True
 
   # pylint: disable=arguments-differ
   def SetUp(self,
             spreadsheet: str,
             sheet_names: List[str],
-            validate_columns: bool = True) -> None:
+            validate_columns: bool) -> None:
     """Sets up a a Google Sheets collector.
 
     Args:
       spreadsheet: ID or URL of the sheet to pull data from
-      sheet_names: List of sheet names inside the spreadsheet to parse. 'All'
-        will parse all sheets inside a spreadsheet.
+      sheet_names: List of sheet names inside the spreadsheet to parse. If not
+        set, all sheets inside the spreadsheet will be parsed.
       validate_columns: Check if mandatory columns required by Timesketch is
         present in the sheets.
     """
@@ -67,30 +67,31 @@ class GoogleSheetsCollector(module.BaseModule):
     if not sheet_names:
       self._all_sheets = True
     self._validate_columns = validate_columns
+    self._sheets_resource = discovery.build('sheets', 'v4',
+      credentials=self._credentials)
 
   def Process(self) -> None:
     """Copies entries from Google Sheets."""
 
     try:
-      # Retrive list of sheets in the spreadsheet
-      service = self._BuildSheetsResource(self._credentials)
+      # Retrieve list of sheets in the spreadsheet
       # Pylint can't see the spreadsheets method.
       # pylint: disable=no-member
-      result = service.spreadsheets().get(
+      result = self._sheets_resource.spreadsheets().get(
           spreadsheetId=self._spreadsheet_id).execute()
       spreadsheet_title = result.get('properties', {}).get('title')
-      sheets = (result.get('sheets', []))
+      sheets = result.get('sheets', [])
 
       for sheet in sheets:
         if not sheet.get('properties'):
           continue
 
-        sheet_title = sheet.get('properties', {}).get('title')
+        sheet_title = sheet.get('properties').get('title')
 
         if not self._all_sheets and sheet_title not in self._sheet_names:
           continue
 
-        self.logger.info('Parsing sheet: {0:s}'.format(sheet_title))
+        self.logger.info(f'Parsing sheet: {sheet_title}')
 
         df = self._ExtractEntiresFromSheet(self._spreadsheet_id, sheet_title)
 
@@ -101,19 +102,17 @@ class GoogleSheetsCollector(module.BaseModule):
             mode='w', delete=False, encoding='utf-8', suffix='.csv')
         output_path = output_file.name
         self.logger.info(
-            'Downloading results of sheet "{0:s}" to {1:s}'.format(
-                sheet_title, output_path))
+            f'Downloading results of sheet "{sheet_title}" to {output_path}')
 
         df.to_csv(index=False, na_rep='NaN', path_or_buf=output_file)
 
         self.logger.success(
-            'Downloaded results of sheet "{0:s}" to {1:s}'.format(
-                sheet_title, output_path))
+            f'Downloaded results of sheet "{sheet_title}" to {output_path}')
         output_file.close()
         sheet_csv_file = containers.File(
             name= self._spreadsheet_id,
             path=output_path,
-            description= '{0:s}_{1:s}'.format(spreadsheet_title, sheet_title))
+            description= f'{spreadsheet_title}_{sheet_title}')
         self.state.StoreContainer(sheet_csv_file)
 
     except (RefreshError, DefaultCredentialsError) as exception:
@@ -143,7 +142,7 @@ class GoogleSheetsCollector(module.BaseModule):
               credentials_path, self.SCOPES)
         except ValueError as exception:
           self.logger.warning(
-              'Unable to load credentials: {0:s}'.format(exception))
+              f'Unable to load credentials: {exception}')
           credentials = None
 
       # If there are no (valid) credentials available, let the user log in.
@@ -158,7 +157,7 @@ class GoogleSheetsCollector(module.BaseModule):
                 'No OAuth application credentials available to access google '
                 'sheets. Please generate OAuth application credentials (see '
                 'https://developers.google.com/sheets/api/guides/authorizing) '
-                'and save them to {0:s}.').format(secrets_path)
+                f'and save them to {secrets_path}.')
             self.ModuleError(error_message, critical=True)
           flow = InstalledAppFlow.from_client_secrets_file(
               secrets_path, self.SCOPES)
@@ -169,18 +168,6 @@ class GoogleSheetsCollector(module.BaseModule):
           token_file.write(credentials.to_json())
 
     return credentials
-
-  def _BuildSheetsResource(self,
-                           credentials: Credentials) -> discovery.Resource:
-    """Builds a Google Sheets resource object to use to request logs.
-
-    Args:
-      credentials: Google API credentials
-
-    Returns:
-      A resouce object for interacting with the Google Sheets API.
-    """
-    return discovery.build('sheets', 'v4', credentials=credentials)
 
   def _ValidateSpreadSheetId(self, spreadsheet: str) -> str:
     """Extract and validate spreadsheet ID.
@@ -194,16 +181,14 @@ class GoogleSheetsCollector(module.BaseModule):
     spreadsheet_match = re.search(r'.*?([01][0-9A-Za-z_-]{20,}).*', spreadsheet)
     if not spreadsheet_match:
       self.ModuleError(
-          'spreadsheet id is not in the correct format {0:s}.'.format(
-              spreadsheet),
+          f'spreadsheet ID is not in the correct format {spreadsheet}.',
           critical=True)
-      return ""
 
     return spreadsheet_match.group(1)
 
   def _ExtractEntiresFromSheet(self,
                                spreadsheet_id: str,
-                               sheet_title: str) -> DataFrame:
+                               sheet_title: str) -> pd.DataFrame:
     """Extract entries from the sheet inside the spreadsheet.
 
     Args:
@@ -211,18 +196,18 @@ class GoogleSheetsCollector(module.BaseModule):
       sheet_title: Title of the sheet inside the spreadsheet to parse.
 
     Returns:
-        Dataframe with entries from sheet inside the spreadsheet
+        Dataframe with entries from sheet inside the spreadsheet or None if the
+        sheet contain no entries.
     """
 
-    resource = self._BuildSheetsResource(self._credentials)
     # Pylint can't see the spreadsheets method.
     # pylint: disable=no-member
-    sheet_content_result = resource.spreadsheets().values().get(
+    sheet_content_result = self._sheets_resource.spreadsheets().values().get(
         spreadsheetId=spreadsheet_id, range=sheet_title).execute()
     values = sheet_content_result.get('values', [])
 
     if not values:
-      self.logger.warning('No data found in sheet "{0:s}".'.format(sheet_title))
+      self.logger.warning(f'No data found in sheet "{sheet_title}".')
       return None
 
     df = pd.DataFrame(values[1:], columns=values[0])
@@ -238,8 +223,8 @@ class GoogleSheetsCollector(module.BaseModule):
     for column in self._mandatory_columns:
       if column not in df.columns:
         self.logger.warning(
-            'Mandatory column "{0:s}" was not found in sheet "{1:s}".'.format(
-                column, sheet_title))
+            f'Mandatory column "{column}" was not found in sheet \
+              "{sheet_title}".')
         self.logger.warning('Please make sure all mandatory columns are \
           present:'
         )
