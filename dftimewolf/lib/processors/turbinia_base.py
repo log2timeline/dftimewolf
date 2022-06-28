c0b2f9e4515ffb03c3f1aa1725c0a8c37c8c40d7
sa3eed3ed <eids@google.com>
2022-06-28 11:48:50 +0100

    wait

--- dftimewolf/lib/processors/turbinia_base.py
+++ dftimewolf/lib/processors/turbinia_base.py
@@ -179,8 +179,6 @@ class TurbiniaProcessorBase(object):
       threat_intel_indicator: list of strings used as regular expressions in
           the Turbinia grepper module.
       yara_rules: List of Yara rule strings to use in the Turbinia Yara module.
-      wait: Wait until the Turbinia request is done.
-
     Returns:
       The Turbinia task data.
     """
