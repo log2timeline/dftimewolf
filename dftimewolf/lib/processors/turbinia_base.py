cd830b0586aedf7a6697eded88e397b55f7fb646
Said Eid <46205691+sa3eed3ed@users.noreply.github.com>
2022-06-30 19:07:20 +0200

    Update turbinia_base.py

--- dftimewolf/lib/processors/turbinia_base.py
+++ dftimewolf/lib/processors/turbinia_base.py
@@ -182,9 +182,8 @@ class TurbiniaProcessorBase(object):
     Returns:
       The Turbinia task data.
     """
-    return self.TurbiniaWait(
-      self.TurbiniaStart(evidence_, threat_intel_indicators, yara_rules)
-      )
+    request_id = self.TurbiniaStart(evidence_, threat_intel_indicators, yara_rules)
+    return self.TurbiniaWait(request_id)
 
   def TurbiniaStart(
     self,
