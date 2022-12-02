c736db3e7cc4ceee661d3b607eba7dde44c051c5
bskeggs <barnaby.skeggs@mandiant.com>
2022-12-02 16:40:13 +0800

    added backoff and delay functionality to manage gcp cloud logging api limits

--- dftimewolf/lib/collectors/gcp_logging.py
+++ dftimewolf/lib/collectors/gcp_logging.py
@@ -16,8 +16,6 @@ from dftimewolf.lib.containers import containers
 from dftimewolf.lib.modules import manager as modules_manager
 from dftimewolf.lib.state import DFTimewolfState
 
-MAX_RETRIES = 10
-
 # Monkey patching the ProtobufEntry because of various issues, notably
 # https://github.com/googleapis/google-cloud-python/issues/7918
 
@@ -42,75 +40,118 @@ class GCPLogsCollector(module.BaseModule):
     super(GCPLogsCollector, self).__init__(state, name=name, critical=critical)
     self._filter_expression = ''
     self._project_name = ''
+    self._backoff = False
+    self._delay = 0
+
+  def OutputFile(self):
+    """Generate an output file name and path"""
+    output_file = tempfile.NamedTemporaryFile(
+        mode='w', delete=False, encoding='utf-8', suffix='.jsonl')
+    output_path = output_file.name
+    self.logger.info('Downloading logs to {0:s}'.format(output_path))
+    return output_file, output_path
+
+  def SetupLoggingClient(self):
+    """Sets up a GCP Logging Client
+    
+    Args:
+      N/A
+    """
+    if self._project_name:
+      return logging.Client(_use_grpc=False,  # type: ignore
+                                        project=self._project_name)
+    else:
+      return logging.Client(_use_grpc=False)  # type: ignore
+  
+  def ListPages(self, logging_client):
+    """Returns pages based on a Cloud Logging filter
+    
+    Args:
+      logging_client: A GCP Cloud Logging client
+    """
+    results = logging_client.list_entries(  # type: ignore
+          order_by=logging.DESCENDING,
+          filter_=self._filter_expression,
+          page_size=1000)
+    return results.pages
+
+  def ProcessPages(self, pages, backoff_multiplier, output_file, output_path):
+    """Iterates through a generator or pages and saves logs to disk.
+    Can optionally perform exponential backoff if query API limits are exceeded.
+    
+    Args:
+      pages (generator): A google cloud logging list_entries generator pages object
+      backoff_multiplier (str): Query delay multiplier if the API quota is met and backoff is enabled
+      output_file (str): Output file name
+      output_path (str): Output file path
+    """
+    while True:
+      try:
+        time.sleep(self._delay)
+        page = next(pages)
+      except google_api_exceptions.TooManyRequests as exception:
+        self.PublishMessage('Hit quota limit requesting GCP logs.')
+        if self._backoff is True:
+          self.PublishMessage('Retrying in 60 seconds with a slower query rate.')
+          self.PublishMessage('Due to the GCP logging API, the query must restart from the beginning')
+          time.sleep(60)
+          if self._delay == 0:
+            self._delay = 1
+          else:
+            self._delay *= backoff_multiplier
+          self.PublishMessage('Setting up new logging client.')
+          logging_client = self.SetupLoggingClient()
+          pages = self.ListPages(logging_client)
+          self.PublishMessage('Restarting query with an API request rate of 1 per {0}s'.format(self._delay))
+          output_file, output_path = self.OutputFile()
+        else:
+          self.PublishMessage('Exponential backoff was not enabled, so query has exited.')
+          self.PublishMessage('The collection is most likely incomplete.')
+      except StopIteration:
+        break
+
+      for entry in page:
+        log_dictionary = entry.to_api_repr()
+        output_file.write(json.dumps(log_dictionary))
+        output_file.write('\n')
+    
+    return output_path
 
   # pylint: disable=arguments-differ
-  def SetUp(self, project_name: str, filter_expression: str) -> None:
+  def SetUp(self, project_name: str, filter_expression: str, backoff: bool, delay: str) -> None:
     """Sets up a a GCP logs collector.
 
     Args:
       project_name (str): name of the project to fetch logs from.
       filter_expression (str): GCP advanced logs filter expression.
+      backoff (bool): Retry queries with an increased delay when API quotas are exceeded.
+      delay (str): Seconds to wait between retreiving results pages to avoid exceeding API quotas
     """
     self._project_name = project_name
     self._filter_expression = filter_expression
+    self._backoff = backoff
+    self._delay = float(delay)
+    self.PublishMessage(f'backoff {self._backoff}')
+    self.PublishMessage(f'delay {self._delay}')
 
   def Process(self) -> None:
     """Copies logs from a cloud project."""
 
-    output_file = tempfile.NamedTemporaryFile(
-        mode='w', delete=False, encoding='utf-8', suffix='.jsonl')
-    output_path = output_file.name
-    self.logger.info(f'Downloading logs to {output_path:s}')
+    output_file, output_path = self.OutputFile()
 
     try:
-      if self._project_name:
-        logging_client = logging.Client(_use_grpc=False,  # type: ignore
-                                        project=self._project_name)
-      else:
-        logging_client = logging.Client(_use_grpc=False)  # type: ignore
+      'Setup a logging client'
+      logging_client = self.SetupLoggingClient()
+      
+      'Get a generator of query results'
+      pages = self.ListPages(logging_client)
 
-      results = logging_client.list_entries(  # type: ignore
-          order_by=logging.DESCENDING,
-          filter_=self._filter_expression,
-          page_size=1000)
-
-      pages = results.pages
-
-      have_results = True
-      while have_results:
-        have_page = False
-        page = []
-        retries = 0
-        while not have_page and retries < MAX_RETRIES:
-          try:
-            page = next(pages)
-            have_page = True
-          except google_api_exceptions.TooManyRequests as exception:
-            retries += 1
-            self.logger.warning(
-                'Hit quota limit requesting GCP logs (retries {0:d} of {1:d}): '
-                '{2:s}'.format(
-                    retries, MAX_RETRIES, str(exception)))
-            time.sleep(4)
-            continue
-          except StopIteration:
-            have_results = False
-            break
-
-        if not have_page and retries >= MAX_RETRIES:
-          self.ModuleError(
-              f'Hit max retries ({MAX_RETRIES:d}) requesting GCP logs',
-              critical=True)
-
-        for entry in page:
-
-          log_dict = entry.to_api_repr()
-          output_file.write(json.dumps(log_dict))
-          output_file.write('\n')
+      'Iterate through query result pages and save json logs to disk'
+      output_path = self.ProcessPages(pages, 2, output_file, output_path)
 
     except google_api_exceptions.NotFound as exception:
       self.ModuleError(
-          f'Error accessing project: {exception!s}', critical=True)
+          'Error accessing project: {0!s}'.format(exception), critical=True)
 
     except google_api_exceptions.InvalidArgument as exception:
       self.ModuleError(
