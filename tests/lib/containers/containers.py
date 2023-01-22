2edfd1cc176dbfab9a9f3beea9ce66fd615fcda5
Thomas Chopitea <tomchop@gmail.com>
2023-01-23 00:37:21 +0100

    Add equality operator on TicketAttribute contaienrs (#704)
    * Add equality operator on TicketAttribute contaienrs
    
    * Fix bugs
    
    * Appease linter
    
    * mypy

--- tests/lib/containers/containers.py
+++ tests/lib/containers/containers.py
@@ -135,6 +135,21 @@ class OsqueryResultDataTest(unittest.TestCase):
 
     self.assertEqual(attribute_names, expected_attribute_names)
 
+class TicketAttributeTest(unittest.TestCase):
+  """Tests for the TicketAttribute attribute container."""
+  def TestEquality(self):
+    """Tests that the equality operator works as intended."""
+    ticket_attribute_container_1 = containers.TicketAttribute(
+      type_='type1', name='name1', value='value1')
+    ticket_attribute_container_2 = containers.TicketAttribute(
+      type_='type1', name='name2', value='value1')
+
+    self.assertNotEqual(
+      ticket_attribute_container_1, ticket_attribute_container_2)
+
+    ticket_attribute_container_2.name = 'name1'
+
+    self.assertEqual(ticket_attribute_container_1, ticket_attribute_container_2)
 
 if __name__ == '__main__':
   unittest.main()
