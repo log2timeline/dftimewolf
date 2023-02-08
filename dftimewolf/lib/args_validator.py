2b32588c085ac61d10f965bdf8b0bbd3b8518fd5
ramo-j <ramoj@google.com>
2023-02-08 04:57:12 +0000

    More validators added to recipes

--- dftimewolf/lib/args_validator.py
+++ dftimewolf/lib/args_validator.py
@@ -219,7 +219,8 @@ class GCPZoneValidator(AbstractValidator):
     'us-east4-b', 'us-east4-c', 'us-east5-a', 'us-east5-b', 'us-east5-c',
     'us-south1-a', 'us-south1-b', 'us-south1-c', 'us-west1-a', 'us-west1-b',
     'us-west1-c', 'us-west2-a', 'us-west2-b', 'us-west2-c', 'us-west3-a',
-    'us-west3-b', 'us-west3-c', 'us-west4-a', 'us-west4-b', 'us-west4-c'
+    'us-west3-b', 'us-west3-c', 'us-west4-a', 'us-west4-b', 'us-west4-c',
+    'global'
   }
   NAME = 'gcp_zone'
 
