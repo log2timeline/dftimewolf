8da6f80151e03ba7b29e377fcaaf2e4377b1550d
Thomas Chopitea <tomchop@gmail.com>
2023-06-14 14:42:33 +0000

    fix tests

--- tests/lib/state.py
+++ tests/lib/state.py
@@ -683,7 +683,7 @@ class StateWithCDMTest(unittest.TestCase):
     with mock.patch('threading.current_thread') as mock_current_thread, \
         mock.patch.object(cdm, 'UpdateModuleStatus') as mock_update_status, \
         mock.patch.object(cdm, 'UpdateModuleThreadState') as module_update_thread_state:
-      mock_current_thread.return_value.getName.return_value = 'ThreadName'
+      mock_current_thread.return_value.name = 'ThreadName'
       test_state.SetupModules()
       test_state.RunModules()
     # pylint: enable=line-too-long
@@ -714,6 +714,14 @@ class StateWithCDMTest(unittest.TestCase):
             'ThreadName', 'three'),
         mock.call('ThreadAwareConsumerModule', Status.COMPLETED,
             'ThreadName', 'three')], True)
+
+    # [
+    #   call('ThreadAwareConsumerModule', <Status.RUNNING: 'Running'>, <MagicMock name='current_thread().name' id='140351567852160'>, 'one'),
+    # call('ThreadAwareConsumerModule', <Status.COMPLETED: 'Completed'>, <MagicMock name='current_thread().name' id='140351567852160'>, 'one'),
+    # call('ThreadAwareConsumerModule', <Status.RUNNING: 'Running'>, <MagicMock name='current_thread().name' id='140351567852160'>, 'two'),
+    # call('ThreadAwareConsumerModule', <Status.COMPLETED: 'Completed'>, <MagicMock name='current_thread().name' id='140351567852160'>, 'two'),
+    # call('ThreadAwareConsumerModule', <Status.RUNNING: 'Running'>, <MagicMock name='current_thread().name' id='140351567852160'>, 'three'),
+    # call('ThreadAwareConsumerModule', <Status.COMPLETED: 'Completed'>, <MagicMock name='current_thread().name' id='140351567852160'>, 'three')]
     self.assertEqual(module_update_thread_state.call_count, 6)
 
     self.assertEqual(mock_threaded_process.call_count, 3)
