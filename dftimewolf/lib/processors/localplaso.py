92f7f3de43c4ca227267c1620400603b755278dc
Thomas Chopitea <tomchop@gmail.com>
2023-07-14 12:15:53 +0000

    Silence mypy errors

--- dftimewolf/lib/processors/localplaso.py
+++ dftimewolf/lib/processors/localplaso.py
@@ -44,12 +44,12 @@ class LocalPlasoProcessor(module.BaseModule):
 
   def _CheckDockerImage(self) -> bool:
     """Checks if an image is available on the local Docker installation."""
-    client = docker.from_env()
+    client = docker.from_env()  # type: ignore
     try:
       # Checks if image exists locally, does not pull from registry.
       client.images.get("log2timeline/plaso:latest")
       return True
-    except docker.errors.ImageNotFound:
+    except docker.errors.ImageNotFound: # type: ignore
       return False
 
   def _DockerPlasoRun(
@@ -65,7 +65,7 @@ class LocalPlasoProcessor(module.BaseModule):
             'mode': 'rw'
         }
     }
-    client = docker.from_env()
+    client = docker.from_env()  # type: ignore
     client.containers.run(
         "log2timeline/plaso:latest", volumes=volumes, command=command)
 
