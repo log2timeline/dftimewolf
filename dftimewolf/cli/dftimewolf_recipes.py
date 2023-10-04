6f6ec9fce6bea72ac9fa2a0af4adbbcc7f25650a
itsmvd <70900075+itsmvd@users.noreply.github.com>
2023-10-04 10:42:56 +0200

    Extract Telemetry from init to its own method (#785)
    * Extract Telemetry from init to its own method
    
    This allows the telemetry object to be loaded with a new configuration after initializing DFTimewolfTool
    
    * Update dftimewolf_recipes.py

--- dftimewolf/cli/dftimewolf_recipes.py
+++ dftimewolf/cli/dftimewolf_recipes.py
@@ -115,8 +115,8 @@ class DFTimewolfTool(object):
     if not workflow_uuid:
       workflow_uuid = str(uuid.uuid4())
     self.uuid = workflow_uuid
-    self.telemetry = telemetry.GetTelemetry(uuid=self.uuid)
 
+    self.InitializeTelemetry()
     self._DetermineDataFilesPath()
 
   @property
@@ -377,6 +377,10 @@ class DFTimewolfTool(object):
   def RecipesManager(self) -> recipes_manager.RecipesManager:
     """Returns the recipes manager."""
     return self._recipes_manager
+
+  def InitializeTelemetry(self) -> None:
+    """Initializes the telemetry object."""
+    self.telemetry = telemetry.GetTelemetry(uuid=self.uuid)
 
 
 def SignalHandler(*unused_argvs: Any) -> None:
