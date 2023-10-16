dfb9a726a798e022d84eb0886f51a079d6211bd7
Thomas Chopitea <tomchop@gmail.com>
2023-10-16 19:02:49 +0200

    Some changes

--- docker/dftimewolf-release/docker-entrypoint.sh
+++ docker/dftimewolf-release/docker-entrypoint.sh
@@ -3,6 +3,10 @@ set -e
 
 PYTHONPATH="/app"
 
+echo "Running entrypoint.sh"
+echo "PYTHONPATH: $PYTHONPATH"
+echo "arg passed: $1"
+
 # Pass all args to dftimewolf
 if [[ "$1" =~ 'dftimewolf' ]]; then
     poetry run dftimewolf "$@"
