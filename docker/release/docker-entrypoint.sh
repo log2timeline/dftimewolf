2db91250f9f2434c4729137b93f6066a0b7ae8d3
Thomas Chopitea <tomchop@gmail.com>
2023-10-19 09:07:17 +0200

    slower but more stable

--- docker/release/docker-entrypoint.sh
+++ docker/release/docker-entrypoint.sh
@@ -5,8 +5,7 @@ PYTHONPATH="/app"
 
 # Pass all args to dftimewolf
 if [[ "$1" =~ 'dftimewolf' ]]; then
-    # poetry run is a bit slower than using the virtualenv directly
-    $PYBIN -m dftimewolf.cli.dftimewolf_recipes "${@:2}"
+    poetry run python -m dftimewolf.cli.dftimewolf_recipes "${@:2}"
 fi
 
 # Run a custom command on container start
