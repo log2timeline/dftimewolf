90db3bfac15d3c4642263bf43fb9c737510d6e67
Thomas Chopitea <tomchop@gmail.com>
2023-10-24 16:47:29 +0000

    Avoid executing "dftimewolf"

--- docker/release/docker-entrypoint.sh
+++ docker/release/docker-entrypoint.sh
@@ -5,8 +5,8 @@ PYTHONPATH="/app"
 
 # Pass all args to dftimewolf
 if [[ "$1" =~ 'dftimewolf' ]]; then
-    poetry run python -m dftimewolf.cli.dftimewolf_recipes "${@:2}"
+    poetry run dftimewolf "${@:2}"
+else
+    # Run a custom command on container start
+    exec "$@"
 fi
-
-# Run a custom command on container start
-exec "$@"
