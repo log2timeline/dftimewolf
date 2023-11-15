366a08cad22f39d101ff123473534353f5b9ba7b
Thomas Chopitea <tomchop@gmail.com>
2023-11-15 15:52:27 +0100

    Fix: jenkins build (#799)
    * Fix pytype
    
    * Upgrade opensearch version
    
    * Don't use the token files
    
    * Pointer to credentials
    
    * Remove unused tokens
    
    * Change directory name
    
    * Add the test config to version control

---
+++ config/e2e/e2e_grr_artifact_hosts.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+# This file is meant to be used by e2e test runners
+
+# Exit on error.
+set -e
+sudo sysctl -w vm.max_map_count=262144
+bash ./config/e2e/jenkins_dftimewolf_install.sh include-docker include-grr include-timesketch include-plaso
+export DFTIMEWOLF_NO_RAINBOW=true
+export DFTIMEWOLF_NO_CURSES=1
+PYTHONPATH=. poetry run python tests/test_dftimewolf.py
+PYTHONPATH=. poetry run python dftimewolf/cli/dftimewolf_recipes.py --help
+PYTHONPATH=. poetry run python dftimewolf/cli/dftimewolf_recipes.py grr_artifact_ts $NODE_NAME test --artifacts LinuxAuditLogs --timesketch_endpoint http://localhost:80/ --timesketch_username test --timesketch_password test
