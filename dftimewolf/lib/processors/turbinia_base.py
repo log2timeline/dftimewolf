b079eb8a2c0f7ee8bcb815e8c330185c7234f7bf
Thomas Chopitea <tomchop@gmail.com>
2024-05-15 13:52:11 +0000

    bugfix

--- dftimewolf/lib/processors/turbinia_base.py
+++ dftimewolf/lib/processors/turbinia_base.py
@@ -214,7 +214,7 @@ class TurbiniaProcessorBase(module.BaseModule):
           extracted_path = self._ExtractFiles(local_path, path)
           if os.path.exists(extracted_path):
             result = extracted_path
-            self.logging.info(f"Extracted output of task {task_id} to {result}")
+            self.logger.info(f"Extracted output of task {task_id} to {result}")
           return result
       except (turbinia_api_lib.exceptions.ApiException,
           turbinia_api_lib.exceptions.UnauthorizedException) as exception:
