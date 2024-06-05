adae59604be49f5ca07ef9e89b7c0a0de0e6c8a2
Daniel White <onager@deerpie.com>
2024-06-05 22:18:12 +0200

    Fix possible issue with empty times in logs container

--- dftimewolf/lib/processors/workspace_audit_timesketch.py
+++ dftimewolf/lib/processors/workspace_audit_timesketch.py
@@ -198,8 +198,16 @@ class WorkspaceAuditTimesketch(BaseModule):
 
     timeline_name = 'Workspace {0:s} logs'.format(
         logs_container.application_name)
-    start_time_string = logs_container.start_time.strftime('%Y-%m-%dT%H:%M:%S')
-    end_time_string = logs_container.end_time.strftime('%Y-%m-%dT%H:%M:%S')
+
+    if logs_container.start_time:
+      start_time_string = logs_container.start_time.strftime(
+          '%Y-%m-%dT%H:%M:%S')
+    else:
+      start_time_string = ''
+    if logs_container.end_time:
+      end_time_string = logs_container.end_time.strftime('%Y-%m-%dT%H:%M:%S')
+    else:
+      end_time_string = ''
     if logs_container.user_key:
       timeline_name = f'{timeline_name} for {logs_container.user_key}'
     if logs_container.start_time:
