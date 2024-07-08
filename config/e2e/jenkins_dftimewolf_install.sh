274e8b36a6730780c5a8417652ebe12b51e58994
Thomas Chopitea <tomchop@gmail.com>
2024-07-08 08:20:11 +0000

    Upgrade jenkins script

--- config/e2e/jenkins_dftimewolf_install.sh
+++ config/e2e/jenkins_dftimewolf_install.sh
@@ -15,18 +15,24 @@ python3 --version
 
 
 if [[ "$*" =~ "include-docker" ]]; then
-    echo "Adding docker key"
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository \
-       "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-       $(lsb_release -cs) \
-       stable"
-    echo "apt update and install docker-ce"
-    sudo apt-get update -qq
-    sudo apt-get install -qq -y docker-ce
-    curl -L https://github.com/docker/compose/releases/download/1.26.1/docker-compose-$(uname -s)-$(uname -m) -o docker-compose
-    sudo cp docker-compose /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
+    # follow instructions at https://docs.docker.com/engine/install/debian/
+    echo "Removing conflicting packages"
+    for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+    echo "adding docker key"
+    sudo apt-get install -y ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+        $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+        sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+
+    echo "install docker packages"
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 fi
 
 if [[ "$*" =~ "include-grr" ]]; then
