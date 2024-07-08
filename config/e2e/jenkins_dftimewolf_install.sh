165b2d235ffad493daf5dcee0a472d1a30ff0ec1
Thomas Chopitea <tomchop@gmail.com>
2024-07-08 08:46:53 +0000

    ubuntu instructions

--- config/e2e/jenkins_dftimewolf_install.sh
+++ config/e2e/jenkins_dftimewolf_install.sh
@@ -17,16 +17,17 @@ python3 --version
 if [[ "$*" =~ "include-docker" ]]; then
     # follow instructions at https://docs.docker.com/engine/install/debian/
     echo "Removing conflicting packages"
-    for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+    for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
     echo "adding docker key"
+    sudo apt-get update
     sudo apt-get install -y ca-certificates curl
     sudo install -m 0755 -d /etc/apt/keyrings
-    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
     sudo chmod a+r /etc/apt/keyrings/docker.asc
 
     echo \
-        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
         $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
         sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
