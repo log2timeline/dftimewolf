4eaebec217424f0baa41bb883baa4f7e7f1efd22
Juan Leaniz <leaniz@google.com>
2024-09-06 09:44:03 -0400

    Add priority_filter flag to artifact processor (#914)
    * Add priority_filter flag to artifact processor
    
    * lint
    
    * Update recipe
    
    * fix recipe

--- dftimewolf/lib/processors/turbinia_artifact.py
+++ dftimewolf/lib/processors/turbinia_artifact.py
@@ -46,7 +46,8 @@ class TurbiniaArtifactProcessor(TurbiniaProcessorBase,
   def SetUp(
       self, project: str, turbinia_recipe: Optional[str],
       turbinia_zone: str, turbinia_api: str, output_directory: str,
-      incident_id: str, sketch_id: str, turbinia_auth: bool = False) -> None:
+      incident_id: str, sketch_id: str, priority_filter: int = 100,
+      turbinia_auth: bool = False) -> None:
     """Sets up the object attributes.
 
     Args:
@@ -70,6 +71,7 @@ class TurbiniaArtifactProcessor(TurbiniaProcessorBase,
         turbinia_api,
         incident_id,
         int(sketch_id) if sketch_id else 0,
+        priority_filter,
         turbinia_auth)
 
   # pytype: disable=signature-mismatch
