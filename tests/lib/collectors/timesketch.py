6c2a40da19e6e2af93182e9a1740650fc07ce2cf
Syd Pleno <syd.pleno@gmail.com>
2024-11-09 03:43:54 +1100

    Initial commit

---
+++ tests/lib/collectors/timesketch.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Tests the TimesketchSearchEventCollectorTest."""
+import unittest
+
+
+class TimesketchSearchEventCollectorTest(unittest.TestCase):
+  """Tests for the TimesketchSearchEventCollector module."""
+
+
+if __name__ == '__main__':
+  unittest.main()
