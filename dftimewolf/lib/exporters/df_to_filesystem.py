97f0d280b5fb222bca300765807030190f9d9b29
ramoj <ramoj@google.com>
2024-11-11 05:54:40 +0000

    Added a Dataframe to Filesystem exporter

---
+++ dftimewolf/lib/exporters/df_to_filesystem.py
@@ -0,0 +1,176 @@
+# -*- coding: utf-8 -*-
+"""Export Dataframes in the state to disk."""
+
+from typing import Optional
+import tempfile
+import os
+import re
+
+from dftimewolf.lib import module
+from dftimewolf.lib.containers import containers
+from dftimewolf.lib.modules import manager as modules_manager
+from dftimewolf.lib.state import DFTimewolfState
+
+
+_JSONL = 'jsonl'
+_CSV = 'csv'
+_MARKDOWN = 'markdown'
+_VALID_OUTPUTS = (_JSONL, _CSV, _MARKDOWN)
+
+
+def _ConvertToValidFilename(filename: str, no_spaces: bool = True) -> str:
+  """Converts a string to a valid filename.
+
+  That is, removes special characters, and replaces them with underscores.
+  Allowed characters are based on https://en.wikipedia.org/wiki/Filename,
+  POSIX "Fully portable filenames".
+
+  Args:
+    filename: The filename to convert.
+    no_spaces: Whether to replace spaces with underscores.
+
+  Returns:
+    A valid filename.
+  """
+  if no_spaces:
+    filename = re.sub(r'\s', '_', filename)
+  if filename[0] == '-':  # Posix filenames cannot start with a hyphen.
+    filename = filename[1:]
+  return re.sub(r'[^a-zA-Z0-9.]', '_', filename)
+
+
+class DataFrameToDiskExporter(module.BaseModule):
+  """Exports pandas Dataframes in the state to the local filesystem."""
+
+  def __init__(
+      self,
+      state: DFTimewolfState,
+      name: Optional[str] = None,
+      critical: bool = False) -> None:
+    super(DataFrameToDiskExporter, self).__init__(
+        state, name=name, critical=critical)
+
+    self._formats: list[str] = []
+    self._output_dir: str = None
+
+  # pylint: disable=arguments-differ
+  def SetUp(self, output_formats: str, output_directory: str) -> None:
+    """Set up the module.
+
+    Args:
+      output_formats: Comma separated formats to export. Supported values are:
+          csv, jsonl, md|markdown. If not specified, 'jsonl' is used.
+      output_directory: Where to write the output. The directory is created if
+          it doesn't already exist.
+    """
+    if output_formats:
+      output_formats = [
+        s.strip().lower() for s in output_formats.split(',') if s]
+      self._formats = list(filter(None, output_formats))
+
+      invalid_formats = []
+      for f in self._formats:
+        if f not in _VALID_OUTPUTS:
+          invalid_formats.append(f)
+      if invalid_formats:
+        self.ModuleError(
+          f'Invalid format(s) specified: {", ".join(invalid_formats)}',
+          critical=True)
+    else:
+      self._formats = [_JSONL]
+
+    self._output_dir = self._VerifyOrCreateOutputDirectory(output_directory)
+
+  def Process(self) -> None:
+    """Perform the exports."""
+    to_export = self.state.GetContainers(containers.DataFrame)
+
+    for df in to_export:
+      self._ExportSingleDataFrame(df)
+
+  def _VerifyOrCreateOutputDirectory(self, directory: str | None) -> None:
+    """Checks for or creates an output directory.
+
+    If the output directory is not specified, then a temp directory will
+    be created. Otherwise, the provided value is checked for existence, and if
+    it doesn't exist, is created.
+
+    Raises:
+      F1CollectorError: If the output path exists already and is not a
+          directory.
+    """
+    if not directory:
+      return tempfile.mkdtemp()
+
+    if os.path.exists(directory):
+      if not os.path.isdir(directory):
+        self.ModuleError(
+          f'Output path {directory} already exists and is not a directory.',
+          critical=True)
+      return directory
+    return os.mkdir(directory)
+
+  def _ExportSingleDataFrame(self, container: containers.DataFrame) -> None:
+    """Export a single Dataframe container.
+
+    Args:
+      df_cont: The dataframe container to export.
+    """
+    if _JSONL in self._formats:
+      self._ExportDataFrameJSONL(container)
+    if _CSV in self._formats:
+      self._ExportDataFrameCSV(container)
+    if _MARKDOWN in self._formats:
+      self._ExportDataFrameMarkdown(container)
+
+  def _ExportDataFrameJSONL(self, container: containers.DataFrame) -> None:
+    """Exports a single dataframe container to a jsonl file."""
+    output_path = os.path.join(
+      self._output_dir, f'{_ConvertToValidFilename(container.name)}.{_JSONL}')
+    self.logger.debug(f'Exporting {container.name} to {output_path}')
+
+    with open(output_path, 'w') as f:
+      container.data_frame.to_json(
+        f, orient='records', lines=True, default_handler=str)
+
+    self.state.StoreContainer(container=containers.File(
+        name=os.path.basename(output_path),
+        path=output_path,
+        description=container.description))
+
+    self.logger.debug(f'Export of {container.name} to {output_path} complete')
+
+  def _ExportDataFrameCSV(self, container: containers.DataFrame) -> None:
+    """Exports a single dataframe container to a csv file."""
+    output_path = os.path.join(
+      self._output_dir, f'{_ConvertToValidFilename(container.name)}.{_CSV}')
+    self.logger.debug(f'Exporting {container.name} to {output_path}')
+
+    with open(output_path, 'w') as f:
+      container.data_frame.to_csv(f, index=False)
+
+    self.state.StoreContainer(container=containers.File(
+        name=os.path.basename(output_path),
+        path=output_path,
+        description=container.description))
+
+    self.logger.debug(f'Export of {container.name} to {output_path} complete')
+
+  def _ExportDataFrameMarkdown(self, container: containers.DataFrame) -> None:
+    """Exports a single dataframe container to a markdown file."""
+    output_path = os.path.join(
+      self._output_dir, f'{_ConvertToValidFilename(container.name)}.md')
+    self.logger.debug(f'Exporting {container.name} to {output_path}')
+
+    with open(output_path, 'w') as f:
+      container.data_frame.to_markdown(f, index=False)
+
+    self.state.StoreContainer(container=containers.File(
+        name=os.path.basename(output_path),
+        path=output_path,
+        description=container.description))
+
+    self.logger.debug(f'Export of {container.name} to {output_path} complete')
+
+
+modules_manager.ModulesManager.RegisterModule(DataFrameToDiskExporter)
---
+++ tests/lib/exporters/df_to_filesystem.py
@@ -0,0 +1,141 @@
+"""Tests for filesystem export of Dataframe containers module."""
+
+import tempfile
+
+from absl.testing import absltest
+from absl.testing import parameterized
+
+import pandas as pd
+
+from dftimewolf import config
+from dftimewolf.lib.containers import containers
+from dftimewolf.lib import state, errors
+from dftimewolf.lib.exporters import df_to_filesystem
+
+
+# pylint: disable=line-too-long
+_INPUT_DF = pd.DataFrame(
+    data=[['2024-11-01T00:00:01', 'value_1', 'value_2', 'value_3'],
+          ['2024-11-01T00:00:02', 'value_4', None, True],
+          ['2024-11-01T00:00:03', 1, 1.1, 1.11111111111],
+          ['2024-11-01T00:00:04',
+           {'key_4': 'value_5', 'key_5': 'value_6'},
+           ['value_7', 'value_8', 'Value_9'],
+           '']],
+    columns=['datetime', 'key_1', 'key_2', 'key_3'])
+
+_EXPECTED_JSONL = """{"datetime":"2024-11-01T00:00:01","key_1":"value_1","key_2":"value_2","key_3":"value_3"}
+{"datetime":"2024-11-01T00:00:02","key_1":"value_4","key_2":null,"key_3":true}
+{"datetime":"2024-11-01T00:00:03","key_1":1,"key_2":1.1,"key_3":1.1111111111}
+{"datetime":"2024-11-01T00:00:04","key_1":{"key_4":"value_5","key_5":"value_6"},"key_2":["value_7","value_8","Value_9"],"key_3":""}
+"""
+
+_EXPECTED_CSV = """datetime,key_1,key_2,key_3
+2024-11-01T00:00:01,value_1,value_2,value_3
+2024-11-01T00:00:02,value_4,,True
+2024-11-01T00:00:03,1,1.1,1.11111111111
+2024-11-01T00:00:04,"{'key_4': 'value_5', 'key_5': 'value_6'}","['value_7', 'value_8', 'Value_9']",
+"""
+
+_EXPECTED_MD = """| datetime            | key_1                                    | key_2                             | key_3         |
+|:--------------------|:-----------------------------------------|:----------------------------------|:--------------|
+| 2024-11-01T00:00:01 | value_1                                  | value_2                           | value_3       |
+| 2024-11-01T00:00:02 | value_4                                  |                                   | True          |
+| 2024-11-01T00:00:03 | 1                                        | 1.1                               | 1.11111111111 |
+| 2024-11-01T00:00:04 | {'key_4': 'value_5', 'key_5': 'value_6'} | ['value_7', 'value_8', 'Value_9'] |               |"""
+# pylint: enable=line-too-long
+
+
+class DataFrameToDiskExporterTest(parameterized.TestCase):
+  """Tests DataFrameToDiskExporter."""
+
+  def setUp(self):
+    """Setup."""
+    self._test_state = state.DFTimewolfState(config.Config)
+    self._module = df_to_filesystem.DataFrameToDiskExporter(self._test_state)
+    self._out_dir = tempfile.mkdtemp()
+
+    self._module.StoreContainer(container=containers.DataFrame(
+      data_frame=_INPUT_DF,
+      description='A test dataframe',
+      name='test_dataframe'))
+
+  def test_Defaults(self):
+    """Tests operation with no params specified."""
+    self._module.SetUp(output_formats='',
+                       output_directory='')
+    self._module.Process()
+
+    out_containers = self._test_state.GetContainers(containers.File)
+    self.assertLen(out_containers, 1)
+
+    with open(out_containers[0].path, 'r') as f:
+      self.assertEqual(f.read(), _EXPECTED_JSONL)
+
+  def test_JSONL(self):
+    """Tests outputting JSONL."""
+    self._module.SetUp(output_formats='jsonl',
+                       output_directory=self._out_dir)
+    self._module.Process()
+
+    out_containers = self._test_state.GetContainers(containers.File)
+    self.assertLen(out_containers, 1)
+
+    with open(out_containers[0].path, 'r') as f:
+      self.assertEqual(f.read(), _EXPECTED_JSONL)
+
+  def test_CSV(self):
+    """Tests outputting CSV."""
+    self._module.SetUp(output_formats='csv',
+                       output_directory=self._out_dir)
+    self._module.Process()
+
+    out_containers = self._test_state.GetContainers(containers.File)
+    self.assertLen(out_containers, 1)
+
+    with open(out_containers[0].path, 'r') as f:
+      self.assertEqual(f.read(), _EXPECTED_CSV)
+
+  def test_Markdown(self):
+    """Tests outputting markdown."""
+    self._module.SetUp(output_formats='markdown',
+                       output_directory=self._out_dir)
+    self._module.Process()
+
+    out_containers = self._test_state.GetContainers(containers.File)
+    self.assertLen(out_containers, 1)
+
+    with open(out_containers[0].path, 'r') as f:
+      self.assertEqual(f.read(), _EXPECTED_MD)
+
+  def test_Multiple(self):
+    """Tests that multiple outputs are generated when specified."""
+    self._module.SetUp(output_formats='jsonl,csv,markdown',
+                       output_directory=self._out_dir)
+    self._module.Process()
+
+    out_containers = self._test_state.GetContainers(containers.File)
+    self.assertLen(out_containers, 3)
+
+    actual_results = []
+    expected_results = (_EXPECTED_JSONL, _EXPECTED_CSV, _EXPECTED_MD)
+
+    for c in out_containers:
+      with open(c.path, 'r') as f:
+        actual_results.append(f.read())
+
+    for r in actual_results:
+      self.assertIn(r, expected_results)
+    for e in expected_results:
+      self.assertIn(e, actual_results)
+
+  def test_InvalidFormat(self):
+    """Tests an error is thrown when an invalid format is selected."""
+    with self.assertRaisesRegex(
+        errors.DFTimewolfError, r'Invalid format\(s\) specified: foobar'):
+      self._module.SetUp(output_formats='jsonl,foobar',
+                         output_directory=self._out_dir)
+
+
+if __name__ == '__main__':
+  absltest.main()
