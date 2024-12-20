23ca274f365173baf24c98d72343c03c4cd94c44
Syd Pleno <syd.pleno@gmail.com>
2024-12-20 17:46:49 +1100

    Remove ollama and vertex_ai modules/tests

--- dftimewolf/lib/processors/llmproviders/__init__.py
+++ dftimewolf/lib/processors/llmproviders/__init__.py
@@ -2,5 +2,3 @@
 """The LLMProviders package."""
 
 from dftimewolf.lib.processors.llmproviders import gemini
-from dftimewolf.lib.processors.llmproviders import ollama
-from dftimewolf.lib.processors.llmproviders import vertex_ai
