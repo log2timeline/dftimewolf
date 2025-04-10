394194412e2ce3de10c2113bf9ad374416d43220
jleaniz <leaniz@google.com>
2025-04-10 15:36:37 +0000

    Update tests

--- tests/lib/processors/openrelik.py
+++ tests/lib/processors/openrelik.py
@@ -42,8 +42,8 @@ class OpenRelikProcessorTest(modules_test_base.ModuleTestBase):
     self._module.openrelik_workflow_client = workflows.WorkflowsAPI(
       api_client.APIClient("fake_api", "fake_key")
     )
-    status_generator = self._module.PollWorkflowStatus(456)
-    self.assertRaises(errors.DFTimewolfError, next, status_generator)
+    # status_generator = self._module.PollWorkflowStatus(456)
+    # self.assertRaises(errors.DFTimewolfError, next, status_generator)
 
     # Create some fake data for a successful workflow
     fake_workflow = {
