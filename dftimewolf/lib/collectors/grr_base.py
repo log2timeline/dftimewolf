169933919872df3ee9095f0639a0c10d7b79b15e
Susanne Westphal <swestphal@google.com>
2025-09-02 11:28:37 +0200

    Revert update of GRR client approval links (#1020)
    GRR open source version was not yet updated, so we keep the links to GRR
    client approvals in dftimewolf in the GitHub version as they were before
    https://github.com/log2timeline/dftimewolf/pull/1016.
    Keep approvals links in one line to patch them when we import the code.

--- dftimewolf/lib/collectors/grr_base.py
+++ dftimewolf/lib/collectors/grr_base.py
@@ -152,8 +152,9 @@ class GRRBaseModule:
         approval_sent = True
         if hasattr(approval, "client_id"):
           approval_url = (
-            f"{self.grr_url}/v2/_clients/{approval.client_id}/approvals/"
-            f"{approval.approval_id}/users/{approval.username}"
+            # pylint: disable=line-too-long
+            f"{self.grr_url}/v2/clients/{approval.client_id}/users/{approval.username}/approvals/{approval.approval_id}"
+            # pylint: enable=line-too-long
           )
         elif hasattr(approval, "hunt_id"):
           approval_url = (
