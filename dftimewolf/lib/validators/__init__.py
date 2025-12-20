edc7e252c4393def47071e77be886a415c1c20ea
Ramo <ramoj@google.com>
2025-12-20 18:05:51 +1100

    Finish pr #1032  (#1033)
    * Add a UUID validator
    
    * Finish pr #1032

--- dftimewolf/lib/validators/__init__.py
+++ dftimewolf/lib/validators/__init__.py
@@ -6,7 +6,8 @@ from dftimewolf.lib.validators import datetime_validator
 from dftimewolf.lib.validators import gcp_zone
 from dftimewolf.lib.validators import grr_host
 from dftimewolf.lib.validators import hostname
+from dftimewolf.lib.validators import integer
 from dftimewolf.lib.validators import regex
 from dftimewolf.lib.validators import subnet
 from dftimewolf.lib.validators import url
-from dftimewolf.lib.validators import integer
+from dftimewolf.lib.validators import uuid
