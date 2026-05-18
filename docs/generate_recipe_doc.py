e7649c4ccd042a367b693b2452e9d2e361c3a462
Ramo <ramoj@google.com>
2026-05-18 14:19:31 +1000

    Update recipe doc generation (#1076)
    * Update recipe doc generation
    
    * Update of recipe docs

--- docs/generate_recipe_doc.py
+++ docs/generate_recipe_doc.py
@@ -18,8 +18,8 @@ This is an auto-generated list of dfTimewolf recipes.
 To regenerate this list, from the repository root, run:
 
 ```
-poetry install -d
-python docs/generate_recipe_doc.py data/recipes
+poetry install
+poetry run python3 docs/generate_recipe_doc.py data/recipes docs/
 ```
 
 ---
