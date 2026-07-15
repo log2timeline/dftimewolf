37c701eff26a6d7c84e1d6fa21219115a50b5584
ramoj <ramoj@google.com>
2026-07-15 03:01:42 +0000

    Remvoe grr modules

--- dftimewolf/lib/validators/__init__.py
+++ dftimewolf/lib/validators/__init__.py
@@ -4,7 +4,6 @@ from dftimewolf.lib.validators import aws_region
 from dftimewolf.lib.validators import azure_region
 from dftimewolf.lib.validators import datetime_validator
 from dftimewolf.lib.validators import gcp_zone
-from dftimewolf.lib.validators import grr_host
 from dftimewolf.lib.validators import hostname
 from dftimewolf.lib.validators import integer
 from dftimewolf.lib.validators import regex
