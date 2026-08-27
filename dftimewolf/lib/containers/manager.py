0f7da1278279f67fdb29259a5fd867793234c82a
Ramo <ramoj@google.com>
2026-08-27 13:05:21 +1000

    Update duplicate container comparison when used for storing dupe containers into the container manager (#1097)
    * Allow container manager to store duplicate containers
    
    * Updates
    
    * Updates
    
    * Add logging
    
    * Updates from review comments
    
    * Linter appeasement

--- dftimewolf/lib/containers/manager.py
+++ dftimewolf/lib/containers/manager.py
@@ -129,7 +129,8 @@ class ContainerManager():
               module.storage[container.CONTAINER_TYPE] = []
 
             # If the container to add exists already in the state, don't add it again
-            if container in [c for c, _ in module.storage[container.CONTAINER_TYPE]]:
+            if any(c is container for c, _ in module.storage[container.CONTAINER_TYPE]):
+              self._logger.debug('Duplicate container not added to storage: %s', str(container))
               continue
             module.storage[container.CONTAINER_TYPE].append((container, source_module))
 
