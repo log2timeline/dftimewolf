770f94c86fbb9dc453afc680aa8e2c8fef9f498b
Ramo <ramoj@google.com>
2026-09-21 16:41:02 +1000

    Update various cloud regions in validators (#1105)
    * Update various cloud regions in validators
    
    * Update test
    
    * Unit test update

--- dftimewolf/lib/validators/aws_region.py
+++ dftimewolf/lib/validators/aws_region.py
@@ -6,20 +6,20 @@ from dftimewolf.lib.validators import manager as validators_manager
 
 # Source:
 #   curl -s https://ip-ranges.amazonaws.com/ip-ranges.json | \
-#   jq -r '.prefixes[] | select(.service == "EC2") | .region' ip-ranges.json \
-#   | sort | uniq
-# Fetched 2023-01-15
+#   jq -r '.prefixes[] | select(.service == "EC2") | .region' | sort | uniq
+# Fetched 2026-09-21
 # TODO - Fetch at runtime?
 REGIONS = frozenset({
-    'af-south-1', 'ap-east-1', 'ap-northeast-1', 'ap-northeast-2',
-    'ap-northeast-3', 'ap-south-1', 'ap-south-2', 'ap-southeast-1',
-    'ap-southeast-2', 'ap-southeast-3', 'ap-southeast-4', 'ap-southeast-6',
-    'ca-central-1', 'ca-west-1', 'cn-north-1', 'cn-northwest-1',
-    'eu-central-1',
-    'eu-central-2', 'eu-north-1', 'eu-south-1', 'eu-south-2', 'eu-west-1',
-    'eu-west-2', 'eu-west-3', 'il-central-1', 'me-central-1', 'me-south-1',
-    'sa-east-1', 'us-east-1', 'us-east-2', 'us-gov-east-1', 'us-gov-west-1',
-    'us-west-1', 'us-west-2'})
+    'GLOBAL', 'af-south-1', 'ap-east-1', 'ap-east-2', 'ap-northeast-1',
+    'ap-northeast-2', 'ap-northeast-3', 'ap-south-1', 'ap-south-2',
+    'ap-southeast-1', 'ap-southeast-2', 'ap-southeast-3', 'ap-southeast-4',
+    'ap-southeast-5', 'ap-southeast-6', 'ap-southeast-7', 'ca-central-1',
+    'ca-west-1', 'cn-north-1', 'cn-northwest-1', 'eu-central-1', 'eu-central-2',
+    'eu-north-1', 'eu-south-1', 'eu-south-2', 'eu-west-1', 'eu-west-2',
+    'eu-west-3', 'eusc-de-east-1', 'il-central-1', 'me-central-1', 'me-south-1',
+    'me-west-1', 'mx-central-1', 'sa-east-1', 'sa-west-1', 'us-east-1',
+    'us-east-2', 'us-gov-east-1', 'us-gov-west-1', 'us-south-1', 'us-west-1',
+    'us-west-2'})
 
 
 class AWSRegionValidator(args_validator.AbstractValidator):
